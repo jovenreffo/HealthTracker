72b0ecec77cd64460e4a4b3227b00bf437fc5f84
0x413 <jovenreffo@gmail.com>
2022-10-27 06:55:00 -0600

    - add stdpath header

---
+++ H/src/StandardPath.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <wx/stdpaths.h>
+#include <wx/log.h>
+
+class StandardPath: protected wxStandardPaths
+{
+public:
+	__forceinline wxString GetExecPath() const { return GetExecutablePath(); }
+	__forceinline wxString GetDataDire() const { return GetLocalDataDir(); }
+	__forceinline wxString GetDocumentsDire() const { return GetDocumentsDir(); }
+};
+
+namespace path_data
+{
+	static StandardPath path;
+	static const wxString execPath = path.GetExecPath();
+	static const wxString dataDir = path.GetDataDire();
+	static const wxString documentsDir = path.GetDocumentsDire();
+}
