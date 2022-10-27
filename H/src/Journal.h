9d5dff0dddaeddddd6a6db79ff3af927c199f002
Joven Reffo <jovenreffo@gmail.com>
2022-10-27 08:30:05 -0600

    added members

--- H/src/Journal.h
+++ H/src/Journal.h
@@ -2,11 +2,30 @@
 #define _JOURNAL_H_
 
 #include <wx/panel.h>
+#include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/listctrl.h>
 
 class Journal : public wxPanel
 {
 private:
 	DECLARE_EVENT_TABLE()
+
+private:
+	wxBoxSizer* m_pTopSizer; // vertical
+	wxBoxSizer* m_pDateSizer; // horizontal
+	wxBoxSizer* m_pEntrySizer; // horizontal
+	wxBoxSizer* m_pTextSizer; // vertical
+	wxBoxSizer* m_pPrevEntrySizer; // vertical
+
+	wxStaticText* m_pDateText;
+	wxStaticText* m_pMessageText;
+	wxStaticText* m_pPrevEntryText;
+
+	wxTextCtrl* m_pTextCtrl;
+	wxListCtrl* m_pListCtrl;
+
+	wxButton* m_pSaveButton;
 
 public:
 	Journal(wxWindow* parent,
@@ -14,6 +33,12 @@ public:
 		const wxPoint& pos = wxDefaultPosition,
 		const wxSize& size = wxDefaultSize,
 		long style = 0L);
+
+	// Window setup
+
+
+	// Events
+
 };
 
 #endif
