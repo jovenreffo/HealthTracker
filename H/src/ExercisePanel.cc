d17d7a776b7180cbb8cf8d73e70600037424372b
0x413 <jovenreffo@gmail.com>
2022-10-28 08:49:52 -0600

    - added event table def to compile

--- H/src/ExercisePanel.cc
+++ H/src/ExercisePanel.cc
@@ -1,5 +1,9 @@
 #include "ExercisePanel.h"
 
+BEGIN_EVENT_TABLE(ExercisePanel, wxPanel)	
+
+END_EVENT_TABLE()
+
 ExercisePanel::ExercisePanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxPanel(parent, id, pos, size, style)
 {
