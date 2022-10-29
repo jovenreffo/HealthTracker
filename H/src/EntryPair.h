4d79bd4f2f2175a5aa1cf1a64bfeb6ef4df19ec4
0x413 <jovenreffo@gmail.com>
2022-10-29 16:05:16 -0600

    EntryPair class

---
+++ H/src/EntryPair.h
@@ -0,0 +1,11 @@
+#ifndef _ENTRYPAIR_H_
+#define _ENTRYPAIR_H_
+
+/*
+* EntryPair is a class designed with the purpose of storing
+* a string-string pair. One string will be the name of the entry, the other will be its contents.
+* However, to avoid storing what may be a large string in a single std::string or wxString object,
+* we will store its contents in an external text file and store the destination of the file in the second value of the pair.
+*/
+
+#endif
