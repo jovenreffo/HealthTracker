00e7bfc3e08dc5dbe5b18aabfef0d505d25242b8
Joven Reffo <jovenreffo@gmail.com>
2022-10-31 09:58:32 -0600

    .

--- H/src/ExerciseBook/WPPanel/WPPanel.cc
+++ H/src/ExerciseBook/WPPanel/WPPanel.cc
@@ -1,5 +1,9 @@
 #include "WPPanel.h"
 
+BEGIN_EVENT_TABLE(WPPanel, wxPanel)
+
+END_EVENT_TABLE()
+
 WPPanel::WPPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxPanel(parent, id, pos, size, style)
 {
--- H/src/ExerciseBook/WPPanel/WPPanel.h
+++ H/src/ExerciseBook/WPPanel/WPPanel.h
@@ -2,10 +2,25 @@
 #define _WPPANEL_H_
 
 #include <wx/panel.h>
+#include <wx/button.h>
+#include <wx/textctrl.h>
+#include <wx/listctrl.h>
+#include <wx/sizer.h>
 
 // WP = workout plan
 class WPPanel: public wxPanel
 {
+private:
+	DECLARE_EVENT_TABLE()
+
+private:
+	wxListCtrl* m_pWorkoutList;
+	wxListCtrl* m_pRoutineList;
+
+	wxButton* m_pHelpButton;
+	wxButton* m_pWorkoutButton;
+	wxButton* m_pRoutineButton;
+
 public:
 	WPPanel(wxWindow* parent,
 		wxWindowID id,
