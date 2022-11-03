40e20eae754429e143f039a50ac4234948d9f670
0x413 <jovenreffo@gmail.com>
2022-11-03 07:01:55 -0600

    - added a splitter window to organize elements

--- H/src/ExerciseBook/WPPanel/WPPanel.cc
+++ H/src/ExerciseBook/WPPanel/WPPanel.cc
@@ -6,7 +6,7 @@ BEGIN_EVENT_TABLE(WPPanel, wxPanel)
 END_EVENT_TABLE()
 
 WPPanel::WPPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
-	: wxSplitterWindow(parent, id, pos, size, style)
+	: wxPanel(parent, id, pos, size, style)
 {
 	this->Init();
 }
@@ -14,25 +14,41 @@ WPPanel::WPPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSi
 void WPPanel::Init()
 {
 	this->SetupControls();
-	this->SetupPanels();
 	this->SetupSplitter();
+	this->SetupPanels();
 }
 
 void WPPanel::SetupControls()
 {
 	m_pWorkoutList = new wxListCtrl(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
 	m_pRoutineList = new wxListCtrl(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
-
+	
 	m_pHelpButton = new wxButton(this, wxID_HELP, _T("Help"), wxDefaultPosition, wxDefaultSize);
 	m_pAddWorkoutButton = new wxButton(this, wxID_ADD, _T("Add Workout"), wxDefaultPosition, wxDefaultSize);
 }
 
 void WPPanel::SetupPanels()
 {
+	m_pWorkoutPanel = new wxPanel(m_pSplitter, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+	m_pRoutinePanel = new wxPanel(m_pSplitter, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+
+	m_pWorkoutSizer = new wxBoxSizer(wxVERTICAL);
+	m_pRoutineSizer = new wxBoxSizer(wxVERTICAL);
+	m_pWorkoutPanel->SetSizerAndFit(m_pWorkoutSizer);
+	m_pRoutinePanel->SetSizerAndFit(m_pRoutineSizer);
+
+	m_pSplitter->SplitVertically(m_pWorkoutPanel, m_pRoutinePanel);
+
+	m_pBoxSizer = new wxBoxSizer(wxVERTICAL);
+	m_pBoxSizer->Add(m_pSplitter, 1, wxEXPAND | wxALL, 5);
+	this->SetSizerAndFit(m_pBoxSizer);
 }
 
 void WPPanel::SetupSplitter()
 {
+	m_pSplitter = new wxSplitterWindow(this, wxID_ANY);
+	m_pSplitter->SetSashGravity(0.5);
+	m_pSplitter->SetMinimumPaneSize(100);
 }
 
 // Events
