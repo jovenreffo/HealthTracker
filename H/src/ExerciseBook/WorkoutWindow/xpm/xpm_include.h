aa11ea8233a56896aa3fe5c7e0222ab316039fd2
0x413 <jovenreffo@gmail.com>
2022-11-04 06:55:18 -0600

    Add resources from wxNoteViewer

---
+++ H/src/ExerciseBook/WorkoutWindow/xpm/xpm_include.h
@@ -0,0 +1,15 @@
+#ifndef _XPMINCLUDE_H_
+#define _XPMINCLUDE_H_
+
+#include "cut.xpm"
+#include "copy.xpm"
+#include "paste.xpm"
+#include "undo.xpm"
+#include "redo.xpm"
+#include "zoomin.xpm"
+#include "zoomout.xpm"
+#include "save.xpm"
+#include "saveas.xpm"
+#include "open.xpm"
+
+#endif
