5c67322e3d979ae9bcfa3b29bcc6375043835b52
0x413 <jovenreffo@gmail.com>
2022-11-06 11:54:41 -0700

    - dev updates

--- H/src/ExerciseBook/WPLists/WPLists.cc
+++ H/src/ExerciseBook/WPLists/WPLists.cc
@@ -1,8 +1,12 @@
+#include <wx/msgdlg.h>
 #include "WPLists.h"
 
 // event table for WorkoutList
 BEGIN_EVENT_TABLE(WorkoutList, wxListView)
-
+	EVT_LIST_ITEM_RIGHT_CLICK(wxID_ANY, WorkoutList::OnRightClick)
+	EVT_LIST_ITEM_ACTIVATED(wxID_ANY, WorkoutList::OnDoubleClick)
+	EVT_MENU(wxID_OPEN, WorkoutList::OnOpen)
+	EVT_MENU(wxID_DELETE, WorkoutList::OnDelete)
 END_EVENT_TABLE()
 
 // event table for RoutineList
@@ -13,11 +17,62 @@ END_EVENT_TABLE()
 WorkoutList::WorkoutList(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxListView{ parent, id, pos, size, style }
 {
+	this->SetupColumn();
+	this->SetupMenu();
 }
 
+void WorkoutList::SetupColumn()
+{
+	wxListItem routineCol;
+	routineCol.SetText(_T("Routines"));
+	routineCol.SetImage(-1); 
+	this->InsertColumn(0, routineCol);
+	this->SetColumnWidth(0, 200);
+}
+
+void WorkoutList::SetupMenu()
+{
+	m_pMenu = new wxMenu();
+	m_pMenu->Append(wxID_OPEN, _T("Open Workout\tCtrl+O"));
+	m_pMenu->AppendSeparator();
+	m_pMenu->Append(wxID_DELETE, _T("Delete"));
+}
+
+void WorkoutList::OnRightClick(wxListEvent& event)
+{
+	this->PopupMenu(m_pMenu);
+}
+
+void WorkoutList::OnDoubleClick(wxListEvent& event)
+{
+
+}
+
+void WorkoutList::OnOpen(wxCommandEvent& event)
+{
+
+}
+
+void WorkoutList::OnDelete(wxCommandEvent& event)
+{
+	// make sure the user wants to delete the selected item
+	if(wxMessageBox(_T("Are you sure you want to delete this item?"), _T("Confirm"), wxYES_NO | wxICON_EXCLAMATION) == wxYES)
+		this->DeleteItem(GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED));
+}
+
+// ====== RoutineList ======
 
 RoutineList::RoutineList(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxListView{ parent, id, pos, size, style }
 {
+	this->SetupColumn();
 }
 
+void RoutineList::SetupColumn()
+{
+	wxListItem workoutCol;
+	workoutCol.SetText(_T("Workouts"));
+	workoutCol.SetImage(-1);
+	this->InsertColumn(0, workoutCol);
+	this->SetColumnWidth(0, 200);
+}
--- H/src/ExerciseBook/WPLists/WPLists.h
+++ H/src/ExerciseBook/WPLists/WPLists.h
@@ -2,20 +2,34 @@
 #define _WORKOUTLIST_H_
 
 #include <wx/listctrl.h>
+#include <wx/menu.h>
 
-#define LIST_STYLE ( wxLC_REPORT | wxLC_SINGLE_SEL )
+#define WPLIST_STYLE ( wxLC_REPORT | wxLC_SINGLE_SEL )
 
 class WorkoutList: public wxListView
 {
 private:
 	DECLARE_EVENT_TABLE()
 
+private:
+	wxMenu* m_pMenu;
+
 public:
 	WorkoutList(wxWindow* parent,
 		wxWindowID id,
 		const wxPoint& pos = wxDefaultPosition,
 		const wxSize& size = wxDefaultSize,
-		long style = LIST_STYLE);
+		long style = WPLIST_STYLE);
+
+	// setup
+	void SetupColumn();
+	void SetupMenu();
+
+	// events
+	void OnRightClick(wxListEvent& event);
+	void OnDoubleClick(wxListEvent& event);
+	void OnOpen(wxCommandEvent& event);
+	void OnDelete(wxCommandEvent& event);
 };
 
 class RoutineList: public wxListView
@@ -28,7 +42,9 @@ public:
 		wxWindowID id,
 		const wxPoint& pos = wxDefaultPosition,
 		const wxSize& size = wxDefaultSize,
-		long style = LIST_STYLE);
+		long style = WPLIST_STYLE);
+
+	void SetupColumn();
 };
 
 #endif
