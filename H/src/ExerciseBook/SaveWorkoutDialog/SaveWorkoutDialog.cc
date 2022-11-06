f98cc48bcf7628ef83be772f2a2062d967e62fe2
0x413 <jovenreffo@gmail.com>
2022-11-06 10:22:00 -0700

    - added dialog sizing

--- H/src/ExerciseBook/SaveWorkoutDialog/SaveWorkoutDialog.cc
+++ H/src/ExerciseBook/SaveWorkoutDialog/SaveWorkoutDialog.cc
@@ -1,15 +1,23 @@
 #include <wx/sizer.h>
 #include "SaveWorkoutDialog.h"
 
+BEGIN_EVENT_TABLE(SaveWorkoutDialog, wxDialog)
+	EVT_BUTTON(wxID_OK, SaveWorkoutDialog::OnOK)
+END_EVENT_TABLE()
+
 SaveWorkoutDialog::SaveWorkoutDialog(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style)
 	: wxDialog(parent, id, title, pos, size, style)
 {
 	this->Init();
+	this->SetMinSize(SWDIALOG_SIZE);
+	this->SetInitialSize(SWDIALOG_SIZE);
+	this->SetMaxSize(SWDIALOG_SIZE_MAX);
 }
 
 void SaveWorkoutDialog::Init()
 {
 	m_pTextCtrl = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize);
+	m_pNameText = new wxStaticText(this, wxID_STATIC, _T("Workout name:"));
 	m_pOk = new wxButton(this, wxID_OK, _T("OK"));
 	m_pCancel = new wxButton(this, wxID_CANCEL, _T("Cancel"));
 
@@ -23,4 +31,31 @@ void SaveWorkoutDialog::SetupControls()
 	wxBoxSizer* pButtonSizer = new wxBoxSizer(wxHORIZONTAL);
 
 	this->SetSizerAndFit(pTopSizer);
+
+	pTextSizer->Add(m_pNameText, 0, wxALIGN_CENTRE_VERTICAL | wxALL, 5);
+	pTextSizer->Add(m_pTextCtrl, 0, wxALIGN_CENTRE_VERTICAL | wxALL, 5);
+
+	pButtonSizer->Add(m_pOk, 0, wxALIGN_CENTRE_VERTICAL | wxALL, 5);
+	pButtonSizer->Add(m_pCancel, 0, wxALIGN_CENTRE_VERTICAL | wxALL, 5);
+
+	pTopSizer->Add(pTextSizer);
+	pTopSizer->Add(new wxStaticLine(this, wxID_STATIC), 0, wxEXPAND | wxALL, 5);
+	pTopSizer->Add(pButtonSizer);
+}
+
+void SaveWorkoutDialog::OnOK(wxCommandEvent& event)
+{
+	// set the value of our wxString member to what the user has entered for later use
+	m_textValue = m_pTextCtrl->GetValue();
+
+	if (Validate() && TransferDataFromWindow())
+	{
+		if (IsModal())
+			EndModal(wxID_OK);
+		else
+		{
+			SetReturnCode(wxID_OK);
+			this->Show(false);
+		}
+	}
 }
--- H/src/ExerciseBook/SaveWorkoutDialog/SaveWorkoutDialog.h
+++ H/src/ExerciseBook/SaveWorkoutDialog/SaveWorkoutDialog.h
@@ -3,17 +3,21 @@
 
 #include <wx/dialog.h>
 #include <wx/statline.h>
+#include <wx/stattext.h>
 #include <wx/textctrl.h>
 #include <wx/button.h>
+
+#define SWDIALOG_SIZE	  (wxSize(250, 150))
+#define SWDIALOG_SIZE_MAX (wxSize(275, 175))
 
 class SaveWorkoutDialog: public wxDialog
 {
 private:
-	DECLARE_CLASS(SaveWorkoutDialog)
 	DECLARE_EVENT_TABLE()
 
 private:
 	wxTextCtrl* m_pTextCtrl;
+	wxStaticText* m_pNameText;
 	wxButton* m_pOk;
 	wxButton* m_pCancel;
 
@@ -24,12 +28,17 @@ public:
 		wxWindowID id,
 		const wxString& title,
 		const wxPoint& pos = wxDefaultPosition,
-		const wxSize& size = wxDefaultSize,
+		const wxSize& size = SWDIALOG_SIZE,
 		long style = wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxCLOSE_BOX);
 
 	// Dialog setup
 	void Init();
 	void SetupControls();
+
+	const wxString GetWorkoutName() const { return m_textValue; }
+
+	// Events
+	void OnOK(wxCommandEvent& event);
 };
 
 #endif
