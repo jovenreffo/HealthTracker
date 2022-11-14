26a98b066df28c3a7d9bce94c530776937d59d9b
0x413 <jovenreffo@gmail.com>
2022-11-13 17:21:14 -0700

    - added image list + independant OpenWorkout() function

--- H/src/WPLists.cc
+++ H/src/WPLists.cc
@@ -1,10 +1,13 @@
 #include <wx/msgdlg.h>
 #include "WPLists.h"
+#include "WorkoutWindow.h"
+#include "StandardPath.hpp"
 
 // event table for WorkoutList
 BEGIN_EVENT_TABLE(WorkoutList, wxListView)
 	EVT_LIST_ITEM_RIGHT_CLICK(wxID_ANY, WorkoutList::OnRightClick)
 	EVT_LIST_ITEM_ACTIVATED(wxID_ANY, WorkoutList::OnDoubleClick)
+	EVT_LIST_ITEM_SELECTED(wxID_ANY, WorkoutList::OnItemSelected)
 	EVT_MENU(wxID_OPEN, WorkoutList::OnOpen)
 	EVT_MENU(wxID_DELETE, WorkoutList::OnDelete)
 END_EVENT_TABLE()
@@ -17,8 +20,25 @@ END_EVENT_TABLE()
 WorkoutList::WorkoutList(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxListView{ parent, id, pos, size, style }
 {
+	this->Init();
+}
+
+void WorkoutList::Init()
+{
+	this->SetupImageList();
 	this->SetupColumn();
 	this->SetupMenu();
+}
+
+void WorkoutList::SetupImageList()
+{
+	m_pImageList = new wxImageList(16, 16);
+
+	m_workoutBmp = wxBitmap(path_data::dataDir + _T("\\Images\\workout_small.png"), wxBITMAP_TYPE_PNG);
+	m_workoutBmp.ResetAlpha();
+
+	m_pImageList->Add(m_workoutBmp);
+	this->AssignImageList(m_pImageList, wxIMAGE_LIST_SMALL);
 }
 
 void WorkoutList::SetupColumn()
@@ -33,10 +53,38 @@ void WorkoutList::SetupColumn()
 void WorkoutList::SetupMenu()
 {
 	m_pMenu = new wxMenu();
-	m_pMenu->Append(wxID_OPEN, _T("Open Workout\tCtrl+O"));
+	m_pMenu->Append(wxID_OPEN, _T("Open Workout"));
 	m_pMenu->AppendSeparator();
 	m_pMenu->Append(wxID_DELETE, _T("Delete"));
 }
+
+void WorkoutList::AddItem(const wxString& name, const wxString& content)
+{
+	// insert an item at index 0, add a name-content pair to the content container
+	this->InsertItem(0, name);
+	m_content.push_back({ name, content });
+}
+
+void WorkoutList::OpenWorkout()
+{
+	WorkoutWindow* pWorkoutWindow = new WorkoutWindow(this, wxID_ANY, this);
+
+	// search the content array to see if an item exists with the currently selected name
+	try
+	{
+		for (auto i{ 0 }; i < m_content.size(); ++i)
+		{
+			if (m_currentItemName == m_content[i].m_name)
+				pWorkoutWindow->OpenWorkout(m_content[i].m_content);
+		}
+	}
+	catch (std::exception& e)
+	{
+		wxLogMessage(_T("No item with that name exists.\n%s"), e.what());
+	}
+}
+
+// Events
 
 void WorkoutList::OnRightClick(wxListEvent& event)
 {
@@ -45,12 +93,18 @@ void WorkoutList::OnRightClick(wxListEvent& event)
 
 void WorkoutList::OnDoubleClick(wxListEvent& event)
 {
+	this->OpenWorkout();
+}
 
+void WorkoutList::OnItemSelected(wxListEvent& event)
+{
+	m_selectionIndex = this->GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED);
+	m_currentItemName = this->GetItemText(m_selectionIndex);
 }
 
 void WorkoutList::OnOpen(wxCommandEvent& event)
 {
-
+	this->OpenWorkout();
 }
 
 void WorkoutList::OnDelete(wxCommandEvent& event)
--- H/src/WPLists.h
+++ H/src/WPLists.h
@@ -3,6 +3,9 @@
 
 #include <wx/listctrl.h>
 #include <wx/menu.h>
+#include <wx/imaglist.h>
+#include <vector>
+#include "EntryList.h"
 
 #define WPLIST_STYLE ( wxLC_REPORT | wxLC_SINGLE_SEL )
 
@@ -14,6 +17,13 @@ private:
 private:
 	wxMenu* m_pMenu;
 
+	wxImageList* m_pImageList;
+	wxBitmap m_workoutBmp;
+
+	std::vector<EntryContent> m_content;
+	wxString m_currentItemName;
+	long m_selectionIndex;
+
 public:
 	WorkoutList(wxWindow* parent,
 		wxWindowID id,
@@ -22,12 +32,20 @@ public:
 		long style = WPLIST_STYLE);
 
 	// setup
+	void Init();
+	void SetupImageList();
 	void SetupColumn();
 	void SetupMenu();
+
+	void AddItem(const wxString& name, const wxString& content);
+	void OpenWorkout();
+
+	const std::vector<EntryContent>& GetContent() const { return m_content; }
 
 	// events
 	void OnRightClick(wxListEvent& event);
 	void OnDoubleClick(wxListEvent& event);
+	void OnItemSelected(wxListEvent& event);
 	void OnOpen(wxCommandEvent& event);
 	void OnDelete(wxCommandEvent& event);
 };
