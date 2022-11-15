c0c28317c6dfd7d3a9f35a52d3519f092cd0a738
0x413 <jovenreffo@gmail.com>
2022-11-15 07:11:24 -0700

    - Updated version

--- H/src/AboutDialog/AboutDialog.cc
+++ H/src/AboutDialog/AboutDialog.cc
@@ -5,7 +5,7 @@ AboutDialog::AboutDialog()
 	m_developers.Add(_T("Joven Reffo"));
 
 	m_info.SetName(_T("Health++"));
-	m_info.SetVersion(_T("1.0.0"));
+	m_info.SetVersion(_T("1.0.0 (Beta)"));
 	m_info.SetDescription(_T("Health++ is program designed for individuals to easily track their health progress, and set goals they can achieve."));
 	m_info.SetCopyright(_T("Copyright (c) 2022 Health++"));
 	m_info.SetDevelopers(m_developers);
