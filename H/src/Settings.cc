5f8b1fd5037a5e87ba8bc75e4b5ef37da6657163
jovenreffo <jovenreffo@gmail.com>
2022-11-18 12:07:23 -0700

    - added functionality to reset items

--- H/src/Settings.cc
+++ H/src/Settings.cc
@@ -1,13 +1,17 @@
 #include "Settings.h"
 
 BEGIN_EVENT_TABLE(Settings, wxPanel)
-
+	EVT_BUTTON(static_cast<int>(SE::ID_RESET_ALL), Settings::OnResetAll)
+	EVT_BUTTON(static_cast<int>(SE::ID_RESET_ENTRIES), Settings::OnResetEntries)
+	EVT_BUTTON(static_cast<int>(SE::ID_RESET_WORKOUTS), Settings::OnResetWorkouts)
+	EVT_BUTTON(static_cast<int>(SE::ID_RESET_ROUTINES), Settings::OnResetRoutines)
 END_EVENT_TABLE()
 
-Settings::Settings(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
-	: wxPanel(parent, id, pos, size, style)
+Settings::Settings(ExercisePanel* pExercisePanel, Journal* pJournal, wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+	: wxPanel(parent, id, pos, size, style), m_pExercisePanel{ pExercisePanel }, m_pJournal{ pJournal }
 {
 	this->Init();
+	m_WPNotebook = m_pExercisePanel->GetExerciseBook()->GetNotebook();
 }
 
 void Settings::Init()
@@ -19,6 +23,7 @@ void Settings::Init()
 void Settings::SetupControls()
 {
 	m_pResetAll = new wxButton(m_pTopParent, static_cast<int>(SE::ID_RESET_ALL), _T("Reset All"), wxDefaultPosition, wxDefaultSize);
+	m_pResetEntries = new wxButton(m_pTopParent, static_cast<int>(SE::ID_RESET_ENTRIES), _T("Reset Entries"), wxDefaultPosition, wxDefaultSize);
 	m_pResetWorkouts = new wxButton(m_pTopParent, static_cast<int>(SE::ID_RESET_WORKOUTS), _T("Reset Workouts"), wxDefaultPosition, wxDefaultSize);
 	m_pResetRoutines = new wxButton(m_pTopParent, static_cast<int>(SE::ID_RESET_ROUTINES), _T("Reset Workouts"), wxDefaultPosition, wxDefaultSize);
 }
@@ -50,6 +55,29 @@ void Settings::AddResetGroup()
 	m_pResetBox->Add(pResetSizer);
 
 	pResetSizer->Add(m_pResetAll, 0, wxALIGN_LEFT | wxALL, 5);
+	pResetSizer->Add(m_pResetEntries, 0, wxALIGN_LEFT | wxALL, 5);
 	pResetSizer->Add(m_pResetWorkouts, 0, wxALIGN_LEFT | wxALL, 5);
 	pResetSizer->Add(m_pResetRoutines, 0, wxALIGN_LEFT | wxALL, 5);
+}
+
+void Settings::OnResetAll(wxCommandEvent& WXUNUSED(event))
+{
+	m_pJournal->GetEntryList()->ResetList();
+	m_WPNotebook->GetWorkoutList()->ResetList();
+	m_WPNotebook->GetRoutineList()->ResetList();
+}
+
+void Settings::OnResetEntries(wxCommandEvent& WXUNUSED(event))
+{
+	m_pJournal->GetEntryList()->ResetList();
+}
+
+void Settings::OnResetWorkouts(wxCommandEvent& WXUNUSED(event))
+{
+	m_WPNotebook->GetWorkoutList()->ResetList();
+}
+
+void Settings::OnResetRoutines(wxCommandEvent& WXUNUSED(event))
+{
+	m_WPNotebook->GetRoutineList()->ResetList();
 }
--- H/src/Settings.h
+++ H/src/Settings.h
@@ -7,10 +7,15 @@
 #include <wx/stattext.h>
 #include <wx/button.h>
 
+#include "WPNotebook.h"
+#include "ExercisePanel.h"
+#include "Journal.h"
+
 // SE, Settings Enum
 enum class SE
 {
 	ID_RESET_ALL,
+	ID_RESET_ENTRIES,
 	ID_RESET_WORKOUTS,
 	ID_RESET_ROUTINES
 };
@@ -26,11 +31,19 @@ private:
 	wxStaticBox* m_pTopParent;
 	
 	wxButton* m_pResetAll;
+	wxButton* m_pResetEntries;
 	wxButton* m_pResetWorkouts;
 	wxButton* m_pResetRoutines;
 
+	// Vars that are passed to the ctor
+	ExercisePanel* m_pExercisePanel;
+	WPNotebook* m_WPNotebook;
+	Journal* m_pJournal;
+
 public:
-	Settings(wxWindow* parent,
+	Settings(ExercisePanel* pExercisePanel,
+		Journal* pJournal,
+		wxWindow* parent,
 		wxWindowID id,
 		const wxPoint& pos = wxDefaultPosition,
 		const wxSize& size = wxDefaultSize,
@@ -46,7 +59,10 @@ public:
 	void AddResetGroup();
 
 	// Events
-
+	void OnResetAll(wxCommandEvent& WXUNUSED(event));
+	void OnResetEntries(wxCommandEvent& WXUNUSED(event));
+	void OnResetWorkouts(wxCommandEvent& WXUNUSED(event));
+	void OnResetRoutines(wxCommandEvent& WXUNUSED(event));
 };
 
 #endif
