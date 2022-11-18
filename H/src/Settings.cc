beb75cf2b1bf2e401f4dfdce0e10b1649bbc0d1c
jovenreffo <jovenreffo@gmail.com>
2022-11-18 09:05:35 -0700

    - Begun developing the settings panel

--- H/src/Settings.cc
+++ H/src/Settings.cc
@@ -7,4 +7,47 @@ END_EVENT_TABLE()
 Settings::Settings(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxPanel(parent, id, pos, size, style)
 {
+	this->Init();
+}
+
+void Settings::Init()
+{
+	this->SetupSizers();
+	this->AddAllControls();
+}
+
+void Settings::SetupControls()
+{
+	m_pResetAll = new wxButton(m_pTopParent, static_cast<int>(SE::ID_RESET_ALL), _T("Reset All"), wxDefaultPosition, wxDefaultSize);
+	m_pResetWorkouts = new wxButton(m_pTopParent, static_cast<int>(SE::ID_RESET_WORKOUTS), _T("Reset Workouts"), wxDefaultPosition, wxDefaultSize);
+	m_pResetRoutines = new wxButton(m_pTopParent, static_cast<int>(SE::ID_RESET_ROUTINES), _T("Reset Workouts"), wxDefaultPosition, wxDefaultSize);
+}
+
+void Settings::SetupSizers()
+{
+	// Set up the top level sizer
+	m_pTopSizer = new wxStaticBoxSizer(wxVERTICAL, this, _T("Settings"));
+	m_pTopParent = m_pTopSizer->GetStaticBox();
+	m_pTopSizer->Layout();
+	this->SetSizerAndFit(m_pTopSizer);
+
+	// Initialise the content sizers and controls before arranging the items
+	m_pResetBox = new wxStaticBoxSizer(wxVERTICAL, m_pTopParent, _T("Reset Items"));
+	
+	m_pTopSizer->Add(m_pResetBox, 0, wxALIGN_LEFT | wxALL, 5);
+
+	this->SetupControls();
+}
+
+void Settings::AddAllControls()
+{
+	this->AddResetGroup();
+}
+
+void Settings::AddResetGroup()
+{
+	m_pResetBox->Add(m_pResetAll, 0, wxALIGN_LEFT | wxALL, 5);
+	m_pResetBox->Add(m_pResetWorkouts, 0, wxALIGN_LEFT | wxALL, 5);
+	m_pResetBox->Add(m_pResetRoutines, 0, wxALIGN_LEFT | wxALL, 5);
+
 }
--- H/src/Settings.h
+++ H/src/Settings.h
@@ -2,11 +2,32 @@
 #define _SETTINGS_H_
 
 #include <wx/panel.h>
+#include <wx/statbox.h>
+#include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/button.h>
 
-class Settings : public wxPanel
+// SE, Settings Enum
+enum class SE
+{
+	ID_RESET_ALL,
+	ID_RESET_WORKOUTS,
+	ID_RESET_ROUTINES
+};
+
+class Settings: public wxPanel
 {
 private:
 	DECLARE_EVENT_TABLE()
+
+private:
+	wxStaticBoxSizer* m_pTopSizer;
+	wxStaticBoxSizer* m_pResetBox;
+	wxStaticBox* m_pTopParent;
+	
+	wxButton* m_pResetAll;
+	wxButton* m_pResetWorkouts;
+	wxButton* m_pResetRoutines;
 
 public:
 	Settings(wxWindow* parent,
@@ -15,7 +36,14 @@ public:
 		const wxSize& size = wxDefaultSize,
 		long style = 0L);
 
-	// Panel setup
+	// Panel/control setup
+	void Init();
+	void SetupControls();
+	void SetupSizers();
+	
+	// Controls
+	void AddAllControls();
+	void AddResetGroup();
 
 	// Events
 
