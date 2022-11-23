c557524dc44493bf67a5daa7c83e814475033d31
0x413 <jovenreffo@gmail.com>
2022-11-23 07:15:38 -0700

    - Set up the `NutritionBook` class to be used here.

--- H/src/NutritionPanel.cc
+++ H/src/NutritionPanel.cc
@@ -3,4 +3,26 @@
 NutritionPanel::NutritionPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxPanel(parent, id, pos, size, style)
 {
+	this->Init();
+}
+
+void NutritionPanel::Init()
+{
+	this->SetupNutritionBook();
+	this->SetupSizers();
+}
+
+void NutritionPanel::SetupNutritionBook()
+{
+	m_pNutrBook = new NutritionBook(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNB_TOP);
+	m_pNutrBook->Show(true);
+}
+
+void NutritionPanel::SetupSizers()
+{
+	m_pBoxSizer = new wxBoxSizer(wxVERTICAL);
+	this->SetSizerAndFit(m_pBoxSizer);
+	
+	// add any elements
+	m_pBoxSizer->Add(m_pNutrBook, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
 }
--- H/src/NutritionPanel.h
+++ H/src/NutritionPanel.h
@@ -2,15 +2,26 @@
 #define _NUTRITIONPANEL_H_
 
 #include <wx/panel.h>
+#include <wx/sizer.h>
+#include "NutritionBook.h"
 
 class NutritionPanel: public wxPanel
 {
+private:
+	NutritionBook* m_pNutrBook;
+	wxBoxSizer* m_pBoxSizer;
+
 public:
 	NutritionPanel(wxWindow* parent,
 		wxWindowID id,
 		const wxPoint& pos = wxDefaultPosition,
 		const wxSize& size = wxDefaultSize,
 		long style = 0L);
+
+	// Setup
+	void Init();
+	void SetupNutritionBook();
+	void SetupSizers();
 };
 
 #endif
