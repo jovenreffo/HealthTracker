88adaa1a746f96864f444cc6474c53d8cbdc7d4c
0x413 <jovenreffo@gmail.com>
2022-11-23 07:15:16 -0700

    - In development of adding functionality to this book.

--- H/src/NutritionBook.cc
+++ H/src/NutritionBook.cc
@@ -1,4 +1,5 @@
 #include "NutritionBook.h"
+#include "StandardPath.hpp"
 
 BEGIN_EVENT_TABLE(NutritionBook, wxNotebook)
 
@@ -11,4 +12,15 @@ NutritionBook::NutritionBook(wxWindow* parent, wxWindowID id, const wxPoint& pos
 
 NutritionBook::~NutritionBook()
 {
+}
+
+void NutritionBook::Init()
+{
+}
+
+void NutritionBook::SetupImageList()
+{
+	m_pImageList = new wxImageList(30, 30);
+
+	this->AssignImageList(m_pImageList);
 }
--- H/src/NutritionBook.h
+++ H/src/NutritionBook.h
@@ -2,11 +2,18 @@
 #define _CALORIEBOOK_H_
 
 #include <wx/notebook.h>
+#include <wx/imaglist.h>
+#include <wx/bitmap.h>
+#include <wx/panel.h>
 
 class NutritionBook: public wxNotebook
 {
 private:
 	DECLARE_EVENT_TABLE()
+
+private:
+	wxImageList* m_pImageList;
+	wxBitmap m_calorieBmp;
 
 public:
 	NutritionBook(wxWindow* parent,
@@ -16,6 +23,10 @@ public:
 		long style = 0L);
 
 	~NutritionBook();
+
+	// Setup
+	void Init();
+	void SetupImageList();
 };
 
 #endif
