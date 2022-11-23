891b078cc725855d2c4a37f105aa8abde51ed0f8
jovenreffo <jovenreffo@gmail.com>
2022-11-23 08:43:36 -0700

    `CaloriePanel` class files.

---
+++ H/src/CaloriePanel.cc
@@ -0,0 +1,6 @@
+#include "CaloriePanel.h"
+
+BEGIN_EVENT_TABLE(CaloriePanel, wxPanel)
+
+END_EVENT_TABLE()
+
---
+++ H/src/CaloriePanel.h
@@ -0,0 +1,20 @@
+#ifndef _CALORIEPANEL_H_
+#define _CALORIEPANEL_H_
+
+#include <wx/panel.h>
+#include <wx/button.h>
+#include <wx/sizer.h>
+
+class CaloriePanel: public wxPanel
+{
+private:
+	DECLARE_EVENT_TABLE()
+
+private:
+
+
+public:
+
+};
+
+#endif
