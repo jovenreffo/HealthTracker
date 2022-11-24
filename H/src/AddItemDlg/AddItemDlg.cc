40a51a9717b610c7ccb39b7aa891a171ccb54e11
jovenreffo <jovenreffo@gmail.com>
2022-11-24 11:40:08 -0700

    General development for `AddItemDlg` class.

--- H/src/AddItemDlg/AddItemDlg.cc
+++ H/src/AddItemDlg/AddItemDlg.cc
@@ -4,3 +4,71 @@ BEGIN_EVENT_TABLE(AddItemDlg, wxDialog)
 
 END_EVENT_TABLE()
 
+AddItemDlg::AddItemDlg(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style)
+	: wxDialog(parent, id, title, pos, size, style)
+{
+	this->Init();
+}
+
+void AddItemDlg::Init()
+{
+	this->SetupControls();
+	this->SetupSizers();
+	this->SetupSizing();
+}
+
+void AddItemDlg::SetupSizing()
+{
+	this->SetMinSize(ADDITEMDLG_SIZE);
+	this->SetInitialSize(ADDITEMDLG_SIZE);
+	this->SetMaxSize(ADDITEMDLG_MAX);
+}
+
+void AddItemDlg::SetupControls()
+{
+	m_pOk = new wxButton(this, wxID_OK, _T("OK"), wxDefaultPosition, wxDefaultSize);
+	m_pCancel = new wxButton(this, wxID_CANCEL, _T("Cancel"), wxDefaultPosition, wxDefaultSize);
+
+	m_pFoodName = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize);
+	m_pCalories = new wxSpinCtrl(this, wxID_ANY, _T("0"), wxDefaultPosition, wxDefaultSize, 0, 0);
+}
+
+void AddItemDlg::SetupSizers()
+{
+	m_pTopSizer = new wxBoxSizer(wxVERTICAL);
+	m_pItemSizer = new wxBoxSizer(wxHORIZONTAL);
+	m_pButtonSizer = new wxBoxSizer(wxHORIZONTAL);
+	this->SetSizerAndFit(m_pTopSizer);
+
+	m_pButtonSizer->Add(m_pOk, wxSizerFlags().Proportion(0).Border(wxALL, 5));
+	m_pButtonSizer->Add(m_pCancel, wxSizerFlags().Proportion(0).Border(wxALL, 5));
+
+	m_pItemSizer->Add(new wxStaticText(this, wxID_STATIC, _T("Item name:")), wxSizerFlags().Proportion(0).Align(wxALIGN_CENTRE_VERTICAL).Border(wxALL, 5));
+	m_pItemSizer->Add(m_pFoodName, wxSizerFlags().Proportion(0).Align(wxALIGN_CENTRE_VERTICAL).Border(wxLEFT, 5));
+	m_pTopSizer->Add(m_pItemSizer);
+
+	m_pTopSizer->Add(new wxStaticLine(this, wxID_STATIC), wxSizerFlags().Proportion(0).Expand().Border(wxALL, 5));
+	m_pTopSizer->Add(m_pButtonSizer);
+}
+
+// Events
+
+void AddItemDlg::OnOK(wxCommandEvent& event)
+{
+	if (Validate() && TransferDataFromWindow())
+	{
+		if (IsModal())
+			EndModal(wxID_OK);
+		else
+		{
+			this->SetReturnCode(wxID_OK);
+			this->Show(false);
+		}
+	}
+}
+
+void AddItemDlg::OnCancel(wxCommandEvent& event)
+{
+	this->SetReturnCode(wxID_CANCEL);
+	this->Show(false);
+}
--- H/src/AddItemDlg/AddItemDlg.h
+++ H/src/AddItemDlg/AddItemDlg.h
@@ -4,6 +4,16 @@
 // Dialog for adding an item to the calorie tracker
 
 #include <wx/dialog.h>
+#include <wx/button.h>
+#include <wx/statline.h>
+#include <wx/spinctrl.h>
+#include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/textctrl.h>
+
+#define ADDITEMDLG_STYLE ( wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxCLOSE_BOX )
+#define ADDITEMDLG_SIZE  ( wxSize(300, 225) )
+#define ADDITEMDLG_MAX	 ( wxSize(350, 275) )
 
 class AddItemDlg: public wxDialog
 {
@@ -11,7 +21,33 @@ private:
 	DECLARE_EVENT_TABLE()
 
 private:
+	wxButton* m_pOk;
+	wxButton* m_pCancel;
 
+	wxTextCtrl* m_pFoodName;
+	wxSpinCtrl* m_pCalories;
+
+	wxBoxSizer* m_pTopSizer;
+	wxBoxSizer* m_pItemSizer;
+	wxBoxSizer* m_pButtonSizer;
+
+public:
+	AddItemDlg(wxWindow* parent,
+		wxWindowID id,
+		const wxString& title,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = ADDITEMDLG_STYLE);
+
+	// Setup
+	void Init();
+	void SetupSizing();
+	void SetupControls();
+	void SetupSizers();
+
+	// Events
+	void OnOK(wxCommandEvent& event);
+	void OnCancel(wxCommandEvent& event);
 };
 
 #endif
