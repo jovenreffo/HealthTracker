519203d1aed9c7bd6e558ebdb4ae82a9a451adf2
0x413 <jovenreffo@gmail.com>
2022-11-24 07:03:55 -0700

    - Initialization and arrangement of the list view and a button for adding new items.

--- H/src/CaloriePanel.cc
+++ H/src/CaloriePanel.cc
@@ -16,12 +16,27 @@ void CaloriePanel::Init()
 	this->SetupSizers();
 }
 
+void CaloriePanel::SetupListView()
+{
+	m_pListView->AppendColumn(_T("Food/Item"));
+	m_pListView->AppendColumn(_T("Calories"));
+}
+
 void CaloriePanel::SetupControls()
 {
+	m_pListView = new wxListView(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLC_REPORT | wxLC_SINGLE_SEL);
+	this->SetupListView();
+
+	m_pAddButton = new wxButton(this, static_cast<int>(C::ID_NEW_ITEM), _T("Add Item"), wxDefaultPosition, wxDefaultSize);
 }
 
 void CaloriePanel::SetupSizers()
 {
+	m_pBoxSizer = new wxBoxSizer(wxVERTICAL);
+	this->SetSizerAndFit(m_pBoxSizer);
+
+	m_pBoxSizer->Add(m_pListView, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+	m_pBoxSizer->Add(m_pAddButton, wxSizerFlags().Proportion(0).Border(wxALL, 5));
 }
 
 // Events
