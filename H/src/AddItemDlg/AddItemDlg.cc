03727b8008fcbe63bde4a9fc56db6a74201f1f13
jovenreffo <jovenreffo@gmail.com>
2022-11-24 10:45:33 -0700

    `AddItemDlg` class added for tracking callories / food consumption.

---
+++ H/src/AddItemDlg/AddItemDlg.cc
@@ -0,0 +1,6 @@
+#include "AddItemDlg.h"
+
+BEGIN_EVENT_TABLE(AddItemDlg, wxDialog)
+
+END_EVENT_TABLE()
+
---
+++ H/src/AddItemDlg/AddItemDlg.h
@@ -0,0 +1,17 @@
+#ifndef _ADDITEMDLG_H_
+#define _ADDITEMDLG_H_
+
+// Dialog for adding an item to the calorie tracker
+
+#include <wx/dialog.h>
+
+class AddItemDlg: public wxDialog
+{
+private:
+	DECLARE_EVENT_TABLE()
+
+private:
+
+};
+
+#endif
