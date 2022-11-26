e7cab50bb4e3a57dcceee8422b2ac4b6ed9b5ef3
0x413 <jovenreffo@gmail.com>
2022-11-26 16:36:52 -0700

    Extra bold.

--- H/src/Font/Font.hpp
+++ H/src/Font/Font.hpp
@@ -9,6 +9,11 @@ namespace Fonts
 	{
 		return wxFont(pt, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false);
 	}
+
+	wxFont GetExtraBoldFont(int pt)
+	{
+		return wxFont(pt, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_EXTRABOLD, false);
+	}
 }
 
 #endif
