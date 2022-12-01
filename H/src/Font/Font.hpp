390510c3b9fc886f91bf7a36a56272e0260e34f9
jovenreffo <jovenreffo@gmail.com>
2022-12-01 11:25:20 -0700

    `__forceinline` and made functions static to be used globally

--- H/src/Font/Font.hpp
+++ H/src/Font/Font.hpp
@@ -5,12 +5,12 @@
 
 namespace Fonts
 {
-	wxFont GetBoldFont(int pt)
+	static __forceinline wxFont GetBoldFont(int pt)
 	{
 		return wxFont(pt, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false);
 	}
 
-	wxFont GetExtraBoldFont(int pt)
+	static __forceinline wxFont GetExtraBoldFont(int pt)
 	{
 		return wxFont(pt, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_EXTRABOLD, false);
 	}
