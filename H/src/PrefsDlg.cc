0330904fc80ef09897853be1f00559eefe6a9cbb
jovenreffo <jovenreffo@gmail.com>
2022-12-01 14:11:42 -0700

    - Custom font picker

--- H/src/PrefsDlg.cc
+++ H/src/PrefsDlg.cc
@@ -22,13 +22,15 @@ namespace lang
 
 enum class Prefs
 {
-	ID_CHECK_FONT
+	ID_CHECK_FONT,
+	ID_FONT_PICKER
 };
 
 class GeneralPagePanel : public wxPanel
 {
 private:
 	wxCheckBox* m_pCheckCustomFont;
+	wxFontPickerCtrl* m_pFontPicker;
 
 public:
 	GeneralPagePanel(wxWindow* parent)
@@ -38,20 +40,24 @@ public:
 		this->SetSizerAndFit(pTopSizer);
 
 		// =============== appearance ===============
+		wxStaticText* pAppearanceText = new wxStaticText(this, wxID_STATIC, _("Appearance"));
+		pAppearanceText->SetFont(Fonts::GetBoldFont(10));
+		pTopSizer->Add(pAppearanceText, wxSizerFlags().CentreHorizontal().Left().Border(wxALL, 5));
+
 		wxFlexGridSizer* pAppearanceSizer = new wxFlexGridSizer(2, wxSize(5, 1));
 		pAppearanceSizer->AddGrowableCol(1);
 		pTopSizer->Add(pAppearanceSizer, wxSizerFlags().Expand());
 
-		wxStaticText* pAppearanceText = new wxStaticText(this, wxID_STATIC, _("Appearance"));
-		pAppearanceText->SetFont(Fonts::GetBoldFont(10));
-		pTopSizer->Add(pAppearanceText, wxSizerFlags().CentreHorizontal().Left());
+		m_pFontPicker = new wxFontPickerCtrl(this, (int)Prefs::ID_FONT_PICKER);
+		m_pCheckCustomFont = new wxCheckBox(this, (int)Prefs::ID_CHECK_FONT, _("Use custom font in text fields:"));
 
-		m_pCheckCustomFont = new wxCheckBox(this, (int)Prefs::ID_CHECK_FONT, _("Use Custom Font"));
-		pAppearanceSizer->Add(m_pCheckCustomFont, wxSizerFlags().Proportion(0).Border(wxALL, 5));
+		pAppearanceSizer->Add(m_pCheckCustomFont, wxSizerFlags().CentreVertical().Expand().Border(wxALL, 5));
+		pAppearanceSizer->Add(m_pFontPicker, wxSizerFlags().CentreVertical().Expand().Border(wxALL, 5));
 
 		// Event binding
 		m_pCheckCustomFont->Bind(wxEVT_CHECKBOX, &GeneralPagePanel::OnUseCustomFont, this);
 
+		this->Fit();
 		this->SetMinSize(wxSize(300, 350));
 	}
 
