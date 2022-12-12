6f035b2705d313756a67f9b6d140c87bef878dea
jovenreffo <jovenreffo@gmail.com>
2022-12-12 13:34:12 -0700

    - added outline for class

--- H/src/WorkoutDialog.h
+++ H/src/WorkoutDialog.h
@@ -1,6 +1,38 @@
 #ifndef _WORKOUTDIALOG_H_
 #define _WORKOUTDIALOG_H_
 
+#include <wx/dialog.h>
+#include <wx/textctrl.h>
+#include <wx/imaglist.h>
+#include <wx/bitmap.h>
 
+#define WD_STYLE ( wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxCLOSE_BOX )
+#define WD_SIZE	 ( wxSize(300, 250) )
+
+class WorkoutDialog: public wxDialog
+{
+private:
+	DECLARE_EVENT_TABLE()
+
+private:
+	wxImageList* m_pImageList;
+
+public:
+	WorkoutDialog(wxWindow* parent,
+		wxWindowID id,
+		const wxString& title,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = WD_STYLE);
+
+	// Window setup
+	void Init();
+	void SetupWindowSizing();
+	void SetupImageList();
+	void SetupControls();
+	void SetupSizers();
+
+	// Events
+};
 
 #endif
