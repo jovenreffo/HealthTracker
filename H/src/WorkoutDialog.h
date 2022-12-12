0824ae7b7f6657135b438019045cb472b411f336
jovenreffo <jovenreffo@gmail.com>
2022-12-12 13:44:32 -0700

    - add more members

--- H/src/WorkoutDialog.h
+++ H/src/WorkoutDialog.h
@@ -5,6 +5,7 @@
 #include <wx/textctrl.h>
 #include <wx/imaglist.h>
 #include <wx/bitmap.h>
+#include <wx/bmpbuttn.h>
 
 #define WD_STYLE ( wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxCLOSE_BOX )
 #define WD_SIZE	 ( wxSize(300, 250) )
@@ -15,7 +16,25 @@ private:
 	DECLARE_EVENT_TABLE()
 
 private:
+	// Visuals
 	wxImageList* m_pImageList;
+	wxBitmap m_cutBmp;
+	wxBitmap m_copyBmp;
+	wxBitmap m_pasteBmp;
+	wxBitmap m_undoBmp;
+	wxBitmap m_redoBmp;
+	wxBitmapButton* m_pCut;
+	wxBitmapButton* m_pCopy;
+	wxBitmapButton* m_pPaste;
+	wxBitmapButton* m_pUndo;
+	wxBitmapButton* m_pRedo;
+
+	// Controls
+	wxTextCtrl* m_pTextCtrl;
+	wxButton* m_pOk;
+	wxButton* m_pCancel;
+
+	// 
 
 public:
 	WorkoutDialog(wxWindow* parent,
