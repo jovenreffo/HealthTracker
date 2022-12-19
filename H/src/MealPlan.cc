c94c6d7e28905e2a7962f1f21c3306fce6905645
0x413 <jovenreffo@gmail.com>
2022-12-19 06:43:16 -0700

    - Pushing before developing a splitter

--- H/src/MealPlan.cc
+++ H/src/MealPlan.cc
@@ -30,18 +30,32 @@ void MealPlan::SetupControls()
 {
 	// Button
 	m_addBmp = wxBitmap(path_data::dataDir + _T("\\Images\\add.png"), wxBITMAP_TYPE_PNG);
-	m_pAddMeal = new wxButton(this, static_cast<int>(MP::ID_ADD_PLAN), _T("Add Task"), wxDefaultPosition, wxDefaultSize, wxNO_BORDER);
+	m_pAddMeal = new wxButton(this, static_cast<int>(MP::ID_ADD_PLAN), _T("Add Meal"), wxDefaultPosition, wxDefaultSize, wxNO_BORDER);
 	m_pAddMeal->SetBitmap(m_addBmp);
 
 	// Lists
 	m_pMealList = new MealList(this, static_cast<int>(MP::ID_MEAL_LIST), wxDefaultPosition, wxDefaultSize);
-	m_pFeaturedList = new wxListView(this, static_cast<int>(MP::ID_FEATURED_LIST), wxDefaultPosition, wxDefaultSize);
-
-	// Sizers
+	m_pFeaturedList = new wxListView(this, static_cast<int>(MP::ID_FEATURED_LIST), wxDefaultPosition, wxDefaultSize, wxLC_REPORT | wxLC_SINGLE_SEL);
 }
 
 void MealPlan::SetupSizers()
 {
+	// Initialization
+	m_pTopSizer = new wxBoxSizer(wxVERTICAL);
+	m_pListSizer = new wxBoxSizer(wxHORIZONTAL); // Horizontal sizer to arrange the 2 statbox sizers side by side
+	m_pPlansList = new wxStaticBoxSizer(wxVERTICAL, this, _T("Meal Plans"));
+	m_pFeaturedPlan = new wxStaticBoxSizer(wxVERTICAL, this, _T("Featured Plan"));
+	this->SetSizerAndFit(m_pTopSizer);
+
+	// Add items to the sizers
+	m_pTopSizer->Add(m_pAddMeal, wxSizerFlags().Left().Border(wxALL, 5));
+	m_pPlansList->Add(m_pMealList, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+	m_pFeaturedPlan->Add(m_pFeaturedList, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+	m_pListSizer->Add(m_pPlansList, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+	m_pListSizer->Add(m_pFeaturedPlan, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+
+	// Add sizers to the top-level
+	m_pTopSizer->Add(m_pListSizer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
 }
 
 // ================== MealList ==================
--- H/src/MealPlan.h
+++ H/src/MealPlan.h
@@ -3,11 +3,41 @@
 
 #include <wx/panel.h>
 #include <wx/listctrl.h>
+#include <wx/bitmap.h>
+#include <wx/button.h>
+#include <wx/sizer.h>
+#include <wx/splitter.h>
+
+enum class MP
+{
+	ID_ADD_PLAN,
+	ID_MEAL_LIST,
+	ID_FEATURED_LIST
+};
+
+class MealList;
 
 class MealPlan : public wxPanel
 {
 private:
 	DECLARE_EVENT_TABLE()
+
+private:
+	// Controls
+	wxButton* m_pAddMeal; wxBitmap m_addBmp;
+	MealList* m_pMealList;
+	wxListView* m_pFeaturedList;
+	
+	// Panels
+	wxSplitterWindow* m_pSplitter;
+	wxPanel* m_pPlansPanel;
+	wxPanel* m_pFeaturedPanel;
+
+	// sizers
+	wxBoxSizer* m_pTopSizer;
+	wxBoxSizer* m_pListSizer;
+	wxStaticBoxSizer* m_pPlansList;
+	wxStaticBoxSizer* m_pFeaturedPlan;
 
 public:
 	MealPlan(wxWindow* parent,
@@ -17,10 +47,25 @@ public:
 		long style = 0L);
 
 	// Panel setup
-
+	void Init();
+	void SetupControls();
+	void SetupSizers();
 
 	// Events
 
+};
+
+class MealList : public wxListView
+{
+private:
+	DECLARE_EVENT_TABLE()
+
+public:
+	MealList(wxWindow* parent,
+		wxWindowID id,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = wxLC_REPORT | wxLC_SINGLE_SEL);
 };
 
 #endif
