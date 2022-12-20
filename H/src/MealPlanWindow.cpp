73279cf99d9ffc4c828f40d3e8eca251f98621b8
jovenreffo <jovenreffo@gmail.com>
2022-12-20 11:38:53 -0700

    add `MealPlanWindow` class

---
+++ H/src/MealPlanWindow.cpp
@@ -0,0 +1,1 @@
+#include "MealPlanWindow.h"
---
+++ H/src/MealPlanWindow.h
@@ -0,0 +1,11 @@
+#ifndef _MEALPLANWINDOW_H_
+#define _MEALPLANWINDOW_H_
+
+#include <wx/window.h>
+
+class MealPlanWindow : public wxWindow
+{
+
+};
+
+#endif
