5b8aef06e43f88ee6bbce92c0508cc5056f850a5
jovenreffo <jovenreffo@gmail.com>
2022-12-20 11:44:51 -0700

    - General setup

--- H/src/MealPlanWindow.cc
+++ H/src/MealPlanWindow.cc
@@ -1,1 +1,15 @@
 #include "MealPlanWindow.h"
+
+BEGIN_EVENT_TABLE(MealPlanWindow, wxFrame)
+
+END_EVENT_TABLE()
+
+MealPlanWindow::MealPlanWindow(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style)
+	: wxFrame(parent, id, title, pos, size, style)
+{
+}
+
+MealPlanWindow::~MealPlanWindow()
+{
+
+}
--- H/src/MealPlanWindow.h
+++ H/src/MealPlanWindow.h
@@ -1,11 +1,21 @@
 #ifndef _MEALPLANWINDOW_H_
 #define _MEALPLANWINDOW_H_
 
-#include <wx/window.h>
+#include <wx/frame.h>
 
-class MealPlanWindow : public wxWindow
+class MealPlanWindow : public wxFrame
 {
+private:
+	DECLARE_EVENT_TABLE()
 
+public:
+	MealPlanWindow(wxWindow* parent = nullptr,
+		wxWindowID id = -1,
+		const wxString& title = _T("Meal Planner"),
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = 0L);
+	~MealPlanWindow();
 };
 
 #endif
