54529e4b8eeee2fe8c54cdfa277e5a2c85bc22e8
0x413 <jovenreffo@gmail.com>
2022-12-21 07:19:04 -0700

    General setup: - Set an icon for the window

--- H/src/MealPlanWindow.cc
+++ H/src/MealPlanWindow.cc
@@ -1,4 +1,5 @@
 #include "MealPlanWindow.h"
+#include "StandardPath.hpp"
 
 BEGIN_EVENT_TABLE(MealPlanWindow, wxFrame)
 
@@ -7,10 +8,26 @@ END_EVENT_TABLE()
 MealPlanWindow::MealPlanWindow(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style)
 	: wxFrame(parent, id, title, pos, size, style)
 {
-
+	this->Init();
 }
 
 MealPlanWindow::~MealPlanWindow()
+{
+}
+
+void MealPlanWindow::Init()
+{
+	this->SetupWindowIcon();
+	this->SetupSizing();
+}
+
+void MealPlanWindow::SetupWindowIcon()
+{
+	m_mealIcon = wxIcon(path_data::dataDir + _T("\\Images\\meal_plan_small.png"), wxBITMAP_TYPE_PNG);
+	this->SetIcon(m_mealIcon);
+}
+
+void MealPlanWindow::SetupSizing()
 {
 
 }
--- H/src/MealPlanWindow.h
+++ H/src/MealPlanWindow.h
@@ -2,11 +2,16 @@
 #define _MEALPLANWINDOW_H_
 
 #include <wx/frame.h>
+#include <wx/bitmap.h>
+#include <wx/icon.h>
 
 class MealPlanWindow : public wxFrame
 {
 private:
 	DECLARE_EVENT_TABLE()
+
+private:
+	wxIcon m_mealIcon;
 
 public:
 	MealPlanWindow(wxWindow* parent = nullptr,
@@ -16,6 +21,11 @@ public:
 		const wxSize& size = wxDefaultSize,
 		long style = wxDEFAULT_FRAME_STYLE);
 	~MealPlanWindow();
+
+	// Setup
+	void Init();
+	void SetupWindowIcon();
+	void SetupSizing();
 };
 
 #endif
