89fb3c7d3d09cdd7e75de18967373bbfa8984b94
jovenreffo <jovenreffo@gmail.com>
2022-12-21 09:04:05 -0700

    - `AddMealDialog` class added - Added more members to MPW

--- H/src/MealPlanWindow.cc
+++ H/src/MealPlanWindow.cc
@@ -2,7 +2,7 @@
 #include "StandardPath.hpp"
 
 BEGIN_EVENT_TABLE(MealPlanWindow, wxFrame)
-
+	EVT_BUTTON(static_cast<int>(MPW::ID_ADD_MEAL), MealPlanWindow::OnAddMeal)
 END_EVENT_TABLE()
 
 MealPlanWindow::MealPlanWindow(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style)
@@ -19,6 +19,7 @@ void MealPlanWindow::Init()
 {
 	this->SetupWindowIcon();
 	this->SetupSizing();
+	this->SetupControls();
 }
 
 void MealPlanWindow::SetupWindowIcon()
@@ -28,6 +29,37 @@ void MealPlanWindow::SetupWindowIcon()
 }
 
 void MealPlanWindow::SetupSizing()
+{
+	this->SetMinSize(MPW_SIZE);
+	this->SetInitialSize(MPW_SIZE);
+}
+
+void MealPlanWindow::SetupControls()
+{
+	m_addBmp = wxBitmap(path_data::dataDir + _T("\\Images\\add.png"), wxBITMAP_TYPE_PNG);
+	m_pAddMeal = new wxButton(this, static_cast<int>(MPW::ID_ADD_MEAL), _T("Add Meal"), wxDefaultPosition, wxDefaultSize, wxNO_BORDER);
+	m_pAddMeal->SetBitmap(m_addBmp);
+}
+
+// events
+
+void MealPlanWindow::OnAddMeal(wxCommandEvent& event)
+{
+
+}
+
+// ========================== AddMealDialog ==========================
+BEGIN_EVENT_TABLE(AddMealDialog, wxDialog)
+
+END_EVENT_TABLE()
+
+AddMealDialog::AddMealDialog(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style)
+	: wxDialog(parent, id, title, pos, size, style)
+{
+
+}
+
+AddMealDialog::~AddMealDialog()
 {
 
 }
--- H/src/MealPlanWindow.h
+++ H/src/MealPlanWindow.h
@@ -3,7 +3,25 @@
 
 #include <wx/frame.h>
 #include <wx/bitmap.h>
+#include <wx/imaglist.h>
 #include <wx/icon.h>
+#include <wx/listctrl.h>
+#include <wx/dialog.h>
+#include <wx/button.h>
+
+#define MPW_SIZE	 ( wxSize(440, 280) )
+#define MPW_MAX_SIZE ( wxSize() )
+
+#define AMD_STYLE	 ( wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxCLOSE_BOX )
+#define AMD_SIZE	 ( wxSize() )
+#define AMD_MAX_SIZE ( wxSize() )
+
+enum MPW
+{
+	ID_ADD_MEAL
+};
+
+class AddMealDialog;
 
 class MealPlanWindow : public wxFrame
 {
@@ -11,14 +29,21 @@ private:
 	DECLARE_EVENT_TABLE()
 
 private:
+	// General window vars
 	wxIcon m_mealIcon;
+
+	// Controls
+	AddMealDialog* m_pAddMealDlg;
+	wxButton* m_pAddMeal; wxBitmap m_addBmp;
+	wxImageList* m_pImageList;
+	wxListView* m_pDayList;
 
 public:
 	MealPlanWindow(wxWindow* parent = nullptr,
 		wxWindowID id = -1,
 		const wxString& title = _T("Meal Planner"),
 		const wxPoint& pos = wxDefaultPosition,
-		const wxSize& size = wxDefaultSize,
+		const wxSize& size = MPW_SIZE,
 		long style = wxDEFAULT_FRAME_STYLE);
 	~MealPlanWindow();
 
@@ -26,6 +51,29 @@ public:
 	void Init();
 	void SetupWindowIcon();
 	void SetupSizing();
+	void SetupControls();
+
+	// Events
+	void OnAddMeal(wxCommandEvent& event);
+};
+
+class AddMealDialog : public wxDialog
+{
+private:
+	DECLARE_EVENT_TABLE()
+
+public:
+	AddMealDialog(wxWindow* parent,
+		wxWindowID id,
+		const wxString& title = _T("Add Meal"),
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = AMD_SIZE,
+		long style = AMD_STYLE);
+	~AddMealDialog();
+
+	// Setup
+
+	// Events
 };
 
 #endif
