768174bd28d219437a9c94901936601dde57c6c6
0x413 <jovenreffo@gmail.com>
2022-12-28 17:41:43 -0700

    - delete item functionality - declarations

--- H/src/MealPlanWindow.cc
+++ H/src/MealPlanWindow.cc
@@ -2,6 +2,7 @@
 #include <wx/valtext.h>
 #include <wx/stattext.h>
 #include <wx/statline.h>
+#include <wx/msgdlg.h>
 #include "MealPlanWindow.h"
 #include "StandardPath.hpp"
 
@@ -26,6 +27,7 @@ void MealPlanWindow::Init()
 	m_pTopPanel = new wxPanel(this);
 	this->SetupWindowIcon();
 	this->SetupControls();
+	this->SetupLists();
 	this->SetupSizers();
 	this->SetupSizing();
 }
@@ -47,23 +49,45 @@ void MealPlanWindow::SetupControls()
 	m_addBmp = wxBitmap(path_data::dataDir + _T("\\Images\\add.png"), wxBITMAP_TYPE_PNG);
 	m_pAddMeal = new wxButton(m_pTopPanel, static_cast<int>(MPW::ID_ADD_MEAL), _T("Add Meal"), wxDefaultPosition, wxDefaultSize, wxNO_BORDER);
 	m_pAddMeal->SetBitmap(m_addBmp);
+
+	m_saveBmp = wxBitmap(path_data::dataDir + _T("\\Images\\save.png"), wxBITMAP_TYPE_PNG);
+	m_pSavePlan = new wxButton(m_pTopPanel, static_cast<int>(MPW::ID_SAVE_PLAN), _T("Save Plan"), wxDefaultPosition, wxDefaultSize, wxNO_BORDER);
+	m_pSavePlan->SetBitmap(m_saveBmp);
 }
 
 void MealPlanWindow::SetupSizers()
 {
 	m_pTopSizer = new wxBoxSizer(wxVERTICAL);
+	m_pButtonSizer = new wxBoxSizer(wxHORIZONTAL);
 	m_pMainSizer = new wxBoxSizer(wxVERTICAL);
 	m_pDaySizer = new wxBoxSizer(wxHORIZONTAL);
 	this->SetSizerAndFit(m_pTopSizer);
 	m_pTopPanel->SetSizerAndFit(m_pMainSizer);
 
+	// Button sizer
+	m_pButtonSizer->Add(m_pAddMeal, wxSizerFlags().Left().Border(wxALL, 5));
+	m_pButtonSizer->Add(m_pSavePlan, wxSizerFlags().Left().Border(wxALL, 5));
+
+	// Add day of week lists to the sizer
+	for (auto i{ 0 }; i < m_pDayList.size(); ++i)
+	{
+		m_pDaySizer->Add(m_pDayList[i], wxSizerFlags().Proportion(1).Expand().Border(wxALL, 1));
+	}
+
 	m_pTopSizer->Add(m_pTopPanel, wxSizerFlags().Proportion(1).Expand());
-	m_pMainSizer->Add(m_pAddMeal, wxSizerFlags().Left().Border(wxALL, 5));
+	m_pMainSizer->Add(m_pButtonSizer, wxSizerFlags().Left().Border(wxALL, 5));
+	m_pMainSizer->Add(m_pDaySizer, wxSizerFlags().Left().Proportion(1).Expand().Border(wxALL, 5));
 }
 
 void MealPlanWindow::SetupLists()
 {
-
+	m_pDayList.push_back(new MealDayList(_T("Sunday"), m_pTopPanel, wxID_ANY));
+	m_pDayList.push_back(new MealDayList(_T("Monday"), m_pTopPanel, wxID_ANY));
+	m_pDayList.push_back(new MealDayList(_T("Tuesday"), m_pTopPanel, wxID_ANY));
+	m_pDayList.push_back(new MealDayList(_T("Wednesday"), m_pTopPanel, wxID_ANY));
+	m_pDayList.push_back(new MealDayList(_T("Thursday"), m_pTopPanel, wxID_ANY));
+	m_pDayList.push_back(new MealDayList(_T("Friday"), m_pTopPanel, wxID_ANY));
+	m_pDayList.push_back(new MealDayList(_T("Saturday"), m_pTopPanel, wxID_ANY));
 }
 
 // events
@@ -72,6 +96,42 @@ void MealPlanWindow::OnAddMeal(wxCommandEvent& event)
 {
 	m_pAddMealDlg = new AddMealDialog(this, wxID_ANY);
 	m_pAddMealDlg->Show(true);
+}
+
+// ========================== SaveMealPlanDialog ==========================
+
+SaveMealPlanDialog::SaveMealPlanDialog(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style)
+	: wxDialog(parent, id, title, pos, size, style)
+{
+	this->Init();
+}
+
+SaveMealPlanDialog::~SaveMealPlanDialog()
+{
+}
+
+void SaveMealPlanDialog::Init()
+{
+	this->SetupControls();
+	this->SetupSizers();
+}
+
+void SaveMealPlanDialog::SetupControls()
+{
+
+}
+
+void SaveMealPlanDialog::SetupSizers()
+{
+
+}
+
+void SaveMealPlanDialog::OnOK(wxCommandEvent& event)
+{
+}
+
+void SaveMealPlanDialog::OnCancel(wxCommandEvent& event)
+{
 }
 
 // ========================== AddMealDialog ==========================
@@ -89,6 +149,7 @@ AddMealDialog::AddMealDialog(wxWindow* parent, wxWindowID id, const wxString& ti
 
 AddMealDialog::~AddMealDialog()
 {
+	// Unbind events
 	m_pOk->Unbind(wxEVT_BUTTON, &AddMealDialog::OnOK, this);
 	m_pOk->Unbind(wxEVT_UPDATE_UI, &AddMealDialog::OnUpdateOK, this);
 	m_pCancel->Unbind(wxEVT_BUTTON, &AddMealDialog::OnCancel, this);
@@ -128,7 +189,6 @@ void AddMealDialog::SetupControls()
 	m_choiceArr.Add(_T("Friday"));
 	m_choiceArr.Add(_T("Saturday"));
 	m_pDayChoice = new wxChoice(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, m_choiceArr, 0, wxGenericValidator(&m_choiceValue));
-
 }
 
 void AddMealDialog::SetupSizers()
@@ -193,5 +253,94 @@ void AddMealDialog::OnUpdateOK(wxUpdateUIEvent& event)
 MealDayList::MealDayList(const wxString& list_title, wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxListView{ parent, id, pos, size, style }, m_listTitle{ list_title }
 {
+	this->Init();
+	this->InsertItem(0, _T("Test Item"), 0);
 
+	// Bind events
+	this->Bind(wxEVT_LIST_ITEM_RIGHT_CLICK, &MealDayList::OnRightClickItem, this);
+	this->Bind(wxEVT_LIST_COL_RIGHT_CLICK, &MealDayList::OnRightClickColumn, this);//
+	m_pMenu->Bind(wxEVT_MENU, &MealDayList::OnDeleteItem, this, wxID_DELETE);
+	m_pMenu->Bind(wxEVT_MENU, &MealDayList::OnViewItem, this, wxID_VIEW_DETAILS);
+	m_pColMenu->Bind(wxEVT_MENU, &MealDayList::OnResetList, this, wxID_RESET);
+}
+
+MealDayList::~MealDayList()
+{
+	// Unbind events
+	this->Unbind(wxEVT_LIST_ITEM_RIGHT_CLICK, &MealDayList::OnRightClickItem, this);
+	this->Unbind(wxEVT_LIST_COL_RIGHT_CLICK, &MealDayList::OnRightClickColumn, this);
+	m_pMenu->Unbind(wxEVT_MENU, &MealDayList::OnDeleteItem, this, wxID_DELETE);
+	m_pMenu->Unbind(wxEVT_MENU, &MealDayList::OnViewItem, this, wxID_VIEW_DETAILS);
+	m_pColMenu->Unbind(wxEVT_MENU, &MealDayList::OnResetList, this, wxID_RESET);
+}
+
+void MealDayList::Init()
+{
+	this->SetupColumn();
+	this->SetupMenu();
+	this->SetupImageList();
+}
+
+void MealDayList::SetupColumn()
+{
+	wxListItem col;
+	col.SetText(m_listTitle);
+	col.SetImage(-1);
+	this->InsertColumn(0, col);
+	//this->SetColumnWidth(0, ...)
+}
+
+void MealDayList::SetupMenu()
+{
+	// Menu for when individual items are right-clicked
+	m_pMenu = new wxMenu();
+	m_pMenu->Append(wxID_VIEW_DETAILS, _T("&View"));
+	m_pMenu->AppendSeparator();
+	m_pMenu->Append(wxID_DELETE, _T("&Delete"));
+
+	// Menu for when the column title is right-clicked
+	m_pColMenu = new wxMenu();
+	m_pColMenu->Append(wxID_RESET, _T("&Reset List"));
+}
+
+void MealDayList::SetupImageList()
+{
+	m_pImageList = new wxImageList(16, 16);
+
+	m_mealBmp = wxBitmap(path_data::dataDir + _T("\\Images\\meal_plan_small.png"), wxBITMAP_TYPE_PNG);
+	m_mealBmp.ResetAlpha();
+
+	m_pImageList->Add(m_mealBmp);
+	this->AssignImageList(m_pImageList, wxIMAGE_LIST_SMALL);
+}
+
+void MealDayList::ResetList()
+{
+	this->DeleteAllItems();
+}
+
+void MealDayList::OnRightClickItem(wxListEvent& event)
+{
+	// Display a pop-up menu for the meals when the user right clicks
+	this->PopupMenu(m_pMenu);
+}
+
+void MealDayList::OnRightClickColumn(wxListEvent& event)
+{
+	this->PopupMenu(m_pColMenu);
+}
+
+void MealDayList::OnDeleteItem(wxCommandEvent& event)
+{
+	if (wxMessageBox(_T("Are you sure you want to delete this item?"), _T("Confirm"), wxYES_NO | wxICON_EXCLAMATION) == wxYES)
+		this->DeleteItem(GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED));
+}
+
+void MealDayList::OnViewItem(wxCommandEvent& event)
+{
+}
+
+void MealDayList::OnResetList(wxCommandEvent& event)
+{
+	this->ResetList();
 }
