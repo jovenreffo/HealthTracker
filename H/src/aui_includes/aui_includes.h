c06c0cd54894355bf342eddb3bde6c76be6e0906
0x413 <jovenreffo@gmail.com>
2023-01-07 09:53:12 -0700

    `aui_includes.h`

---
+++ H/src/aui_includes/aui_includes.h
@@ -0,0 +1,11 @@
+#ifndef AUI_INCLUDES_H
+#define AUI_INCLUDES_H
+
+#include <wx/aui/aui.h>
+#include <wx/aui/auibar.h>
+#include <wx/aui/auibook.h>
+#include <wx/aui/floatpane.h>
+#include <wx/aui/framemanager.h>
+#include <wx/aui/tabart.h>
+
+#endif
