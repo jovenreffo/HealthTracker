1c5e4d52d177f8b4455de7e0cf2a99911d5135f4
0x413 <jovenreffo@gmail.com>
2023-01-18 06:46:02 -0700

    - Change to `MealInfo` - Added day of week info

--- H/src/MealPlanInfo/MealPlanInfo.hpp
+++ H/src/MealPlanInfo/MealPlanInfo.hpp
@@ -4,44 +4,46 @@
 #include <wx/wx.h>
 #include <vector>
 
-/* MealPair
-* The MealPair struct will be used for storing a string-string pair.
+/* MealInfo
+* The MealInfo struct will be used for storing a string-string pair.
 * The first string will be used for holding the meal name, and the second for holding the description.
 */
 
-struct MealPair
+struct MealInfo
 {
 	wxString m_mealName;
 	wxString m_mealDesc;
+	wxString m_dayString;
+	int m_dayIndex;
 };
 
 /* MealPlanInfo
-* The MealPlanInfo class will be used for storing vectors of MealPair/string-string pairs that correspond to their respective days.
+* The MealPlanInfo class will be used for storing vectors of MealInfo/string-string pairs that correspond to their respective days.
 * Several vectors will be instantiated, one for each day of the week. Because vectors allow for dynamic resizing, they are an obious choice.
 */
 
 class MealPlanInfo
 {
 private:
-	std::vector<MealPair> m_sunday;
-	std::vector<MealPair> m_monday;
-	std::vector<MealPair> m_tuesday;
-	std::vector<MealPair> m_wednesday;
-	std::vector<MealPair> m_thursday;
-	std::vector<MealPair> m_friday;
-	std::vector<MealPair> m_saturday;
+	std::vector<MealInfo> m_sunday;
+	std::vector<MealInfo> m_monday;
+	std::vector<MealInfo> m_tuesday;
+	std::vector<MealInfo> m_wednesday;
+	std::vector<MealInfo> m_thursday;
+	std::vector<MealInfo> m_friday;
+	std::vector<MealInfo> m_saturday;
 
 public:
 	MealPlanInfo() = default;
 
 	// Getters
-	const std::vector<MealPair>& GetSunday() const { return m_sunday; }
-	const std::vector<MealPair>& GetMonday() const { return m_monday; }
-	const std::vector<MealPair>& GetTuesday() const { return m_tuesday; }
-	const std::vector<MealPair>& GetWednesday() const { return m_wednesday; }
-	const std::vector<MealPair>& GetThursday() const { return m_thursday; }
-	const std::vector<MealPair>& GetFriday() const { return m_friday; }
-	const std::vector<MealPair>& GetSaturday() const { return m_saturday; }
+	const std::vector<MealInfo>& GetSunday() const { return m_sunday; }
+	const std::vector<MealInfo>& GetMonday() const { return m_monday; }
+	const std::vector<MealInfo>& GetTuesday() const { return m_tuesday; }
+	const std::vector<MealInfo>& GetWednesday() const { return m_wednesday; }
+	const std::vector<MealInfo>& GetThursday() const { return m_thursday; }
+	const std::vector<MealInfo>& GetFriday() const { return m_friday; }
+	const std::vector<MealInfo>& GetSaturday() const { return m_saturday; }
 };
 
 #endif
