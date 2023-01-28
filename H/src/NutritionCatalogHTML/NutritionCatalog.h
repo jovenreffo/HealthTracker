d0c6a503c78e7ce03b04e2b90c7a0af7b1184bcd
0x413 <jovenreffo@gmail.com>
2023-01-28 09:34:58 -0700

    `NutritionCatalog` setup

--- H/src/NutritionCatalogHTML/NutritionCatalog.h
+++ H/src/NutritionCatalogHTML/NutritionCatalog.h
@@ -5,14 +5,24 @@
 #include <wx/html/htmlwin.h>
 #include <wx/wxhtml.h>
 
-class NutritionCatalog : public wxHtmlWindow
+#define NC_SIZE (wxSize(640, 480))
+
+class NutritionCatalog : public wxFrame
 {
+private:
+	wxHtmlWindow* m_pHtmlWindow;
+
 public:
 	NutritionCatalog(wxWindow* parent,
 		wxWindowID id,
+		const wxString& title,
 		const wxPoint& pos = wxDefaultPosition,
-		const wxSize& size = wxDefaultSize,
-		long style = wxHW_DEFAULT_STYLE);
+		const wxSize& size = NC_SIZE,
+		long style = wxDEFAULT_FRAME_STYLE);
+
+	void Init();
+	void SetupSizing();
+
 };
 
 #endif
