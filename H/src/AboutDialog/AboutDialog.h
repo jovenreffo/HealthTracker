6ab98077d28d2d8d23329d6a8624c9dc05888872
0x413 <jovenreffo@gmail.com>
2023-01-28 10:46:09 -0700

    `AboutDialog` new declarations

--- H/src/AboutDialog/AboutDialog.h
+++ H/src/AboutDialog/AboutDialog.h
@@ -2,15 +2,31 @@
 #define _ABOUTDIALOG_H_
 
 #include <wx/aboutdlg.h>
+#include <wx/dialog.h>
+#include <wx/html/htmlwin.h>
+#include <wx/wxhtml.h>
 
-class AboutDialog
+#define AD_SIZE (wxSize(400, 375))
+
+class AboutDialog: public wxDialog
 {
 private:
 	wxAboutDialogInfo m_info;
 	wxArrayString m_developers;
 
 public:
-	AboutDialog();
+	AboutDialog(wxWindow* parent,
+		wxWindowID id,
+		const wxString& title = _T("About"),
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = AD_SIZE,
+		long style = wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxCLOSE_BOX);
+
+	void SetupSizing();
+
+	// events
+	void OnOK(wxCommandEvent& event);
+	void OnLinkClicked(wxHtmlLinkEvent& link);
 };
 
 #endif
