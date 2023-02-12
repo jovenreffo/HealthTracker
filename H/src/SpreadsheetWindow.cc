4a86a3c938c98985059ba02653e0db37ebe19b11
0x413 <jovenreffo@gmail.com>
2023-02-12 09:00:19 -0700

    - Function signatures

--- H/src/SpreadsheetWindow.cc
+++ H/src/SpreadsheetWindow.cc
@@ -1,2 +1,39 @@
 #include "SpreadsheetWindow.h"
 
+SpreadsheetWindow::SpreadsheetWindow(wxWindow* parent,
+	wxWindowID id,
+	const wxString& title,
+	const wxPoint& pos,
+	const wxSize& size,
+	long style)
+	: wxFrame(parent, id, title, pos, size, style)
+{
+
+}
+
+bool SpreadsheetWindow::Create(wxWindow* parent,
+	wxWindowID id,
+	const wxString& title,
+	const wxPoint& pos,
+	const wxSize& size,
+	long style)
+{
+	wxFrame::Create(parent, id, title, pos, size, style);
+
+	return true;
+}
+
+void SpreadsheetWindow::Init()
+{
+
+}
+
+void SpreadsheetWindow::SetupControls()
+{
+
+}
+
+void SpreadsheetWindow::SetupSizers()
+{
+
+}
