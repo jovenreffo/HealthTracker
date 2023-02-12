7c6762feb91ddfb1455df143887372c8e9734cb2
0x413 <jovenreffo@gmail.com>
2023-02-12 08:00:44 -0700

    - Add new `SpreadsheetWindow` class files

---
+++ H/src/SpreadsheetWindow.cc
@@ -0,0 +1,2 @@
+#include "SpreadsheetWindow.h"
+
---
+++ H/src/SpreadsheetWindow.h
@@ -0,0 +1,22 @@
+#ifndef SPREADSHEETWINDOW_H
+#define SPREADSHEETWINDOW_H
+
+#include <wx/frame.h>
+#include <wx/wx.h>
+
+// Spreadsheet window style and size macros
+#define SSW_STYLE ( wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxMINIMIZE_BOX | wxMAXIMIZE_BOX | wxCLOSE_BOX )
+#define SSW_SIZE	 ( wxSize(675, 450) )
+
+class SpreadsheetWindow : public wxFrame
+{
+public:
+	SpreadsheetWindow(wxWindow* parent,
+		wxWindowID id = -1,
+		const wxString& title = _T("Spreadsheet"),
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = SSW_SIZE,
+		long style = SSW_STYLE);
+};
+
+#endif
