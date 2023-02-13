d61e0da4c3666037a7b573b187358ce379117a80
0x413 <jovenreffo@gmail.com>
2023-02-13 07:40:48 -0700

    - `SetupSizing()` dec

--- H/src/SpreadsheetWindow.h
+++ H/src/SpreadsheetWindow.h
@@ -2,14 +2,17 @@
 #define SPREADSHEETWINDOW_H
 
 #include <wx/frame.h>
+#include <wx/sizer.h>
 #include <wx/wx.h>
 
 // Spreadsheet window style and size macros
 #define SSW_STYLE ( wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxMINIMIZE_BOX | wxMAXIMIZE_BOX | wxCLOSE_BOX )
-#define SSW_SIZE	 ( wxSize(675, 450) )
+#define SSW_SIZE ( wxSize(675, 450) )
 
 class SpreadsheetWindow : public wxFrame
 {
+private:
+
 public:
 	SpreadsheetWindow(wxWindow* parent,
 		wxWindowID id = -1,
@@ -27,6 +30,7 @@ public:
 		long style = SSW_STYLE);
 
 	void Init();
+	void SetupSizing();
 	void SetupControls();
 	void SetupSizers();
 };
