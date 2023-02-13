12240fff6e50ea4f294487303e192b3ff4fb3f28
0x413 <jovenreffo@gmail.com>
2023-02-13 07:51:07 -0700

    - Files now `Planning`

---
+++ H/src/Planning.cc
@@ -0,0 +1,83 @@
+#include <wx/valtext.h>
+#include "Planning.h"
+#include "StandardPath.hpp"
+
+Calendar::Calendar(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+	: wxNotebook(parent, id, pos, size, style, _T("calendar"))
+{
+	this->Init();
+}
+
+void Calendar::Init()
+{
+	this->SetupImageList();
+	this->SetupPages();
+}
+
+void Calendar::SetupImageList()
+{
+	m_pImageList = new wxImageList(30, 30);
+
+	m_calendarBmp = wxBitmap(path_data::dataDir + _T("\\Images\\calendar_small.png"), wxBITMAP_TYPE_PNG);
+	m_calendarBmp.ResetAlpha();
+
+	m_todoBmp = wxBitmap(path_data::dataDir + _T("\\Images\\tasklist.png"), wxBITMAP_TYPE_PNG);
+	m_todoBmp.ResetAlpha();
+
+	m_pImageList->Add(m_calendarBmp);
+	m_pImageList->Add(m_todoBmp);
+	this->AssignImageList(m_pImageList);
+}
+
+void Calendar::SetupPages()
+{
+	m_pCalendarPanel = new CalendarPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+	m_pTodoPanel = new TodoPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+
+	// Set up pages in the notebook
+	this->AddPage(m_pCalendarPanel, _T("Calendar"), true, 0);
+	this->AddPage(m_pTodoPanel, _T("To-Do List"), false, 1);
+}
+
+// =========================================== CalendarPanel ===========================================
+
+CalendarPanel::CalendarPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+	: wxPanel(parent, id, pos, size, style, _T("caloriepanel"))
+{
+}
+
+// =========================================== TodoPanel ===========================================
+
+TodoPanel::TodoPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+	: wxPanel(parent, id, pos, size, style, _T("todopanel"))
+{
+	this->Init();
+}
+
+// Panel setup
+void TodoPanel::Init()
+{
+	this->SetupControls();
+	this->SetupSizers();
+}
+
+void TodoPanel::SetupControls()
+{
+	m_addBmp = wxBitmap(path_data::dataDir + _T("\\Images\\add.png"), wxBITMAP_TYPE_PNG);
+	m_pAddButton = new wxButton(this, static_cast<int>(TD::ID_ADD_TODO), _T("Add Task"), wxDefaultPosition, wxDefaultSize);
+	m_pAddButton->SetBitmap(m_addBmp);
+}
+
+void TodoPanel::SetupSizers()
+{
+
+}
+
+// Splitter setup
+
+
+// Events
+void TodoPanel::OnTextEnter(wxCommandEvent& event)
+{
+
+}
---
+++ H/src/Planning.h
@@ -0,0 +1,93 @@
+#ifndef _CALENDAR_H_
+#define _CALENDAR_H_
+
+#include <wx/panel.h>
+#include <wx/notebook.h>
+#include <wx/bitmap.h>
+#include <wx/imaglist.h>
+#include <wx/stattext.h>
+#include <wx/statbox.h>
+#include <wx/sizer.h>
+#include <wx/textctrl.h>
+#include <wx/button.h>
+#include <wx/splitter.h>
+
+enum class TD
+{
+	ID_ADD_TODO
+};
+
+class CalendarPanel;
+class TodoPanel;
+
+class Calendar: public wxNotebook
+{
+private:
+
+private:
+	// Visuals
+	wxImageList* m_pImageList;
+	wxBitmap m_calendarBmp;
+	wxBitmap m_todoBmp;
+
+	// Content panels
+	CalendarPanel* m_pCalendarPanel;
+	TodoPanel* m_pTodoPanel;
+
+public:
+	Calendar(wxWindow* parent,
+		wxWindowID id,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = 0L);
+	
+	// Book setup
+	void Init();
+	void SetupImageList();
+	void SetupPages();
+};
+
+class CalendarPanel: public wxPanel
+{
+private:
+
+public:
+	CalendarPanel(wxWindow* parent,
+		wxWindowID id,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = 0L);
+};
+
+class TodoPanel: public wxPanel
+{
+private:
+
+private:
+	// Controls
+	wxButton* m_pAddButton; wxBitmap m_addBmp;
+	wxString m_objectiveStr;
+
+
+public:
+	TodoPanel(wxWindow* parent,
+		wxWindowID id,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = 0L);
+
+	// Other
+	const wxString& GetObjective() const { return m_objectiveStr; }
+
+	// Panel setup
+	void Init();
+	void SetupControls();
+	void SetupSizers();
+
+	// Splitter setup
+
+	// Events
+	void OnTextEnter(wxCommandEvent& event);
+};
+
+#endif
