d9ffb08bc6f2f553cfe50dac54e0bd025ddd0df0
0x413 <jovenreffo@gmail.com>
2023-03-24 07:10:30 -0600

    - more declarations and setup

--- H/src/EditTaskDlg.h
+++ H/src/EditTaskDlg.h
@@ -3,4 +3,36 @@
 
 #include <wx/wx.h>
 
+#define ETD_SIZE (wxSize(325, 275))
+
+class EditTaskDlg : public wxDialog
+{
+private:
+	wxString m_taskName;
+	wxString m_taskDesc;
+
+	wxTextCtrl* m_pTaskName;
+	wxTextCtrl* m_pTaskDesc; // wxTE_MULTILINE
+
+public:
+	EditTaskDlg() = default;
+	EditTaskDlg(const wxString& taskName,
+		const wxString& taskDesc,
+		wxWindow* parent,
+		wxWindowID id,
+		const wxString& title = _T("Edit Task"),
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxCLOSE_BOX);
+	~EditTaskDlg();
+
+	// Dialog setup
+	void Init();
+	void SetupControls();
+	void SetupSizers();
+	void SetupSizing();
+
+	// Eventgs
+};
+
 #endif
