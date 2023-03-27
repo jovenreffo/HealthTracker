ec7d6c656ef5b65c144699b4b69c834f8759c574
0x413 <jovenreffo@gmail.com>
2023-03-27 15:30:23 -0600

    - update labels on dialog OK

--- H/src/Planning.cc
+++ H/src/Planning.cc
@@ -432,7 +432,12 @@ void TodoItem::OnEditTask(wxCommandEvent& e)
 
 	if (m_pEditTaskDlg->ShowModal() == wxID_OK)
 	{
+		// Set the values of strings and labels
+		m_taskName = m_pEditTaskDlg->GetTaskName();
+		m_taskDesc = m_pEditTaskDlg->GetTaskDesc();
 
+		m_pItemName->SetLabel(m_taskName);
+		m_pItemDesc->SetLabel(m_taskDesc);
 	}
 }
 
