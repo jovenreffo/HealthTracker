b033f34cad27bda20ee4b1de358a346eb4dc0da6
0x413 <jovenreffo@gmail.com>
2023-04-01 08:30:25 -0600

    - defs and dev

---
+++ H/src/NutritionCatalog.cc
@@ -0,0 +1,97 @@
+#include "NutritionCatalog.h"
+#include "StandardPath.hpp"
+
+// =================== CatalogItem ===================
+
+CatalogItem::CatalogItem(const wxString& name, const wxBitmap& bmp, int calorieCount)
+	: m_name{ name }, m_itemBmp{ bmp }, m_calorieCount{ calorieCount }
+{
+
+}
+
+// =================== FoodList ===================
+
+FoodList::FoodList(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+	: wxListView(parent, id, pos, size, style)
+{
+	this->Init();
+}
+
+FoodList::~FoodList()
+{
+
+}
+
+void FoodList::Init()
+{
+	this->SetupMenu();
+	this->SetupImageList();
+}
+
+void FoodList::SetupMenu()
+{
+	m_pMenu = new wxMenu();
+}
+
+void FoodList::SetupImageList()
+{
+	m_pImageList = new wxImageList(80, 80);
+
+
+	this->AssignImageList(m_pImageList, wxIMAGE_LIST_NORMAL);
+}
+
+void FoodList::OnDoubleClickItem(wxListEvent& e)
+{
+
+}
+
+void FoodList::OnRightClickItem(wxListEvent& e)
+{
+	this->PopupMenu(m_pMenu);
+}
+
+// =================== NutritionCatalog ===================
+
+NutritionCatalog::NutritionCatalog(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style)
+	: wxFrame(parent, id, title, pos, size, style)
+{
+	this->SetBackgroundColour(wxColour(240, 240, 240));
+
+	// Initialization handler
+	this->Init();
+
+#if wxUSE_STATUSBAR
+	this->CreateStatusBar(1);
+	this->SetStatusText(_T("No item selected."));
+#endif
+}
+
+void NutritionCatalog::Init()
+{
+	this->SetupSizing();
+	this->SetupControls();
+	this->SetupSizers();
+}
+
+void NutritionCatalog::SetupControls()
+{
+	m_pFoodList = new FoodList(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+}
+
+void NutritionCatalog::SetupSizers()
+{
+	m_pTopSizer = new wxBoxSizer(wxVERTICAL);
+	this->SetSizerAndFit(m_pTopSizer);
+
+	m_pTopSizer->Add(m_pFoodList, wxSizerFlags().Proportion(1).Expand());
+}
+
+void NutritionCatalog::SetupSizing()
+{
+#ifdef NC_SIZE
+	this->SetInitialSize(NC_SIZE);
+	this->SetMinSize(NC_SIZE);
+#endif
+}
+
