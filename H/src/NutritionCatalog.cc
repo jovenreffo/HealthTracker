7b152b9bb2463946ae9312bb3476b57430ac53cc
0x413 <jovenreffo@gmail.com>
2023-04-10 16:01:39 -0600

    - general dev

--- H/src/NutritionCatalog.cc
+++ H/src/NutritionCatalog.cc
@@ -1,46 +1,93 @@
+#include <wx/statline.h>
 #include "NutritionCatalog.h"
 #include "StandardPath.hpp"
 
 // =================== ItemViewer ===================
 
-#define IV_DLG_SIZE (wxSize(300, 300))
-#define IV_DLG_SIZE_MAX (wxSize())
-
 class ItemViewer : public wxDialog
 {
 private:
+	// Item stuff
+	CatalogItem m_catalogItem;
+	wxString m_itemName;
+	wxBitmap m_itemBmp;
+
+	// Dialog stuff
+	wxButton* m_pAdd; // adding the food item to the nutrition log
+	wxButton* m_pClose;
+	
 	wxBoxSizer* m_pTopSizer;
+	wxBoxSizer* m_pButtonSizer;
 
 public:
-	ItemViewer(wxWindow* parent,
+	ItemViewer(CatalogItem catalogItem,
+		wxWindow* parent,
 		wxWindowID id = -1,
 		const wxString& title = _T("View Nutrition Item"),
 		const wxPoint& pos = wxDefaultPosition,
 		const wxSize& size = wxDefaultSize,
 		long style = wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxCLOSE_BOX)
-		: wxDialog(parent, id, title, pos, size, style)
+		: wxDialog(parent, id, title, pos, size, style), m_catalogItem{ catalogItem }
 	{
+		// Set the title to include the item which is being viewed
+		this->SetTitle(wxString(_T("View Nutritional Item - ")) << m_catalogItem.GetName());
+
 		this->Init();
+
+		// Bind events
 	}
 
 	~ItemViewer() {}
 
 	void Init()
 	{
+		// Set variable information
+		m_itemName = m_catalogItem.GetName();
+		m_itemBmp = m_catalogItem.GetBmp();
+
+		this->SetupControls();
 		this->SetupSizers();
 		this->SetupSizing();
 	}
 
+	void SetupControls()
+	{
+		// Buttons
+		m_pAdd = new wxButton(this, wxID_ADD, _T("Add to Nutrition Log"), wxDefaultPosition, wxDefaultSize);
+		m_pClose = new wxButton(this, wxID_CLOSE, _T("Close"), wxDefaultPosition, wxDefaultSize);
+	}
+
 	void SetupSizers()
 	{
+		m_pTopSizer = new wxBoxSizer(wxVERTICAL);
+		m_pButtonSizer = new wxBoxSizer(wxHORIZONTAL);
+		this->SetSizerAndFit(m_pTopSizer);
 
+		m_pButtonSizer->Add(m_pAdd, wxSizerFlags().CentreVertical().Border(wxALL, 5));
+		m_pButtonSizer->Add(m_pClose, wxSizerFlags().CentreVertical().Border(wxALL, 5));
+
+		m_pTopSizer->Add(new wxStaticLine(this, wxID_STATIC), wxSizerFlags().Expand().Border(wxALL, 5));
+		m_pTopSizer->Add(m_pButtonSizer);
 	}
 
 	void SetupSizing()
 	{
-		this->SetInitialSize(IV_DLG_SIZE);
-		this->SetMinSize(IV_DLG_SIZE);
-		this->SetMaxSize(IV_DLG_SIZE_MAX);
+		wxSize sz = this->DoGetBestSize();
+
+		this->SetInitialSize(sz);
+		this->SetMinSize(sz);
+		this->SetMaxSize( wxSize(sz.x + 200, sz.y + 150) );
+	}
+
+	// Events
+	void OnClose(wxCommandEvent& event)
+	{
+
+	}
+
+	void OnAddToNutritionLog(wxCommandEvent& event)
+	{
+
 	}
 };
 
@@ -64,15 +111,21 @@ FoodList::FoodList(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wx
 	this->AddNutritionItem(ci);
 
 	// Bind events
+	this->Bind(wxEVT_LIST_ITEM_SELECTED, &FoodList::OnSelectItem, this);
 	this->Bind(wxEVT_LIST_ITEM_RIGHT_CLICK, &FoodList::OnRightClickItem, this);
 	this->Bind(wxEVT_LIST_ITEM_ACTIVATED, &FoodList::OnDoubleClickItem, this);
+
+	m_pMenu->Bind(wxEVT_MENU, &FoodList::OnOpenItem, this, wxID_OPEN);
 }
 
 FoodList::~FoodList()
 {
 	// Unbind events
+	this->Unbind(wxEVT_LIST_ITEM_SELECTED, &FoodList::OnSelectItem, this);
 	this->Unbind(wxEVT_LIST_ITEM_RIGHT_CLICK, &FoodList::OnRightClickItem, this);
 	this->Unbind(wxEVT_LIST_ITEM_ACTIVATED, &FoodList::OnDoubleClickItem, this);
+
+	m_pMenu->Unbind(wxEVT_MENU, &FoodList::OnOpenItem, this, wxID_OPEN);
 }
 
 void FoodList::Init()
@@ -98,7 +151,7 @@ void FoodList::SetupImageList()
 
 void FoodList::AddNutritionItem(const CatalogItem& info)
 {
-	//m_pCatalogItems->push_back(info);
+	m_catalogItems.push_back(info);
 
 	wxString name = info.GetName();
 	wxBitmap bmp = info.GetBmp();
@@ -108,9 +161,26 @@ void FoodList::AddNutritionItem(const CatalogItem& info)
 	this->SetItemImage(0, 0);
 }
 
+void FoodList::OpenItem()
+{
+	m_pItemViewer = new ItemViewer(m_catalogItems[0], this);
+	m_pItemViewer->Show(true);
+
+	if (m_pItemViewer->ShowModal() == wxID_OK)
+	{
+
+	}
+}
+
+void FoodList::OnSelectItem(wxListEvent& event)
+{
+	m_selectionIndex = GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED);
+	m_currentItemName = GetItemText(m_selectionIndex, 0);
+}
+
 void FoodList::OnDoubleClickItem(wxListEvent& e)
 {
-
+	this->OpenItem();
 }
 
 void FoodList::OnRightClickItem(wxListEvent& e)
@@ -120,7 +190,7 @@ void FoodList::OnRightClickItem(wxListEvent& e)
 
 void FoodList::OnOpenItem(wxCommandEvent& event)
 {
-
+	this->OpenItem();
 }
 
 // =================== NutritionCatalog ===================
@@ -129,6 +199,7 @@ NutritionCatalog::NutritionCatalog(wxWindow* parent, wxWindowID id, const wxStri
 	: wxFrame(parent, id, title, pos, size, style)
 {
 	this->SetBackgroundColour(wxColour(240, 240, 240));
+	this->SetIcon(wxIcon(path_data::dataDir + _T("\\Images\\catalog.png"), wxBITMAP_TYPE_PNG));
 
 	// Initialization handler
 	this->Init();
