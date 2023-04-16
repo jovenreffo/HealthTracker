4e5c8e52900fa49bb56c44609717fa2742b32df6
0x413 <jovenreffo@gmail.com>
2023-04-16 17:27:21 -0600

    .

---
+++ H/src/CalorieList.h
@@ -0,0 +1,45 @@
+#ifndef CALORIELIST_H
+#define CALORIELIST_H
+
+#include <wx/wx.h>
+
+#include "Nutrient Total/Total.hpp"
+#include "AddItemDlg.h"
+
+class CalorieList : public wxListView
+{
+private:
+	wxMenu* m_pMenu;
+	wxImageList* m_pImageList;
+	wxBitmap m_calorieBmp;
+
+	Total m_total;
+
+public:
+	CalorieList(wxWindow* parent,
+		wxWindowID id,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = wxLC_REPORT | wxLC_SINGLE_SEL);
+	~CalorieList();
+
+	Total GetTotal() const { return m_total; }
+	void AddItem(const wxString& item, AddItemDlg* pAddItemDlg);
+	void UpdateTotal();
+	void HandleDeleteItem();
+
+	// Setup
+	void Init();
+	void SetupMenu();
+	void SetupColumns();
+	void SetupImageList();
+	void SetupTotalItem();
+
+	// Events
+	void OnRightClick(wxListEvent& event);
+	void OnDoubleClick(wxListEvent& event);
+	void OnDeleteItem(wxCommandEvent& event);
+	void OnKey(wxKeyEvent& event);
+};
+
+#endif
