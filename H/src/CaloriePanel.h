4a375e60b71477e649bbcbf59f1fabb03b54c81b
0x413 <jovenreffo@gmail.com>
2023-04-16 17:27:29 -0600

    .

--- H/src/CaloriePanel.h
+++ H/src/CaloriePanel.h
@@ -9,8 +9,9 @@
 #include <wx/listctrl.h>
 #include <wx/bmpbuttn.h>
 
-#include "AddItemDlg/AddItemDlg.h"
+#include "AddItemDlg.h"
 #include "NutritionCatalog.h"
+#include "CalorieList.h"
 
 enum class CP
 {
@@ -18,8 +19,6 @@ enum class CP
 	ID_NEW_ITEM,
 	ID_TOTAL
 };
-
-class CalorieList;
 
 class CaloriePanel: public wxPanel
 {
@@ -56,44 +55,6 @@ public:
 	void OnSearch(wxCommandEvent& event);
 	void OnNewItem(wxCommandEvent& event);
 	void OnSaveInformation(wxCommandEvent& event);
-};
-
-class CalorieList: public wxListView
-{
-private:
-	wxMenu* m_pMenu;
-	wxImageList* m_pImageList;
-	wxBitmap m_calorieBmp;
-	CaloriePanel* m_pCaloriePanel;
-
-	Total m_total;
-
-public:
-	CalorieList(CaloriePanel* pCaloriePanel,
-		wxWindow* parent,
-		wxWindowID id,
-		const wxPoint& pos = wxDefaultPosition,
-		const wxSize& size = wxDefaultSize,
-		long style = wxLC_REPORT | wxLC_SINGLE_SEL);
-	~CalorieList();
-
-	Total GetTotal() const { return m_total; }
-	void AddItem(const wxString& item, AddItemDlg* pAddItemDlg);
-	void UpdateTotal();
-	void HandleDeleteItem();
-
-	// Setup
-	void Init();
-	void SetupMenu();
-	void SetupColumns();
-	void SetupImageList();
-	void SetupTotalItem();
-
-	// Events
-	void OnRightClick(wxListEvent& event);
-	void OnDoubleClick(wxListEvent& event);
-	void OnDeleteItem(wxCommandEvent& event);
-	void OnKey(wxKeyEvent& event);
 };
 
 #endif
