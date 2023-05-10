5cea003c5a9591838070d74e5b54ad94d9d03244
0x413 <jovenreffo@gmail.com>
2023-05-10 07:25:00 -0600

    `TimedExerciseList`

---
+++ H/src/TimedExerciseList.h
@@ -0,0 +1,12 @@
+#ifndef TIMEDEXERCISELIST_H
+#define TIMEDEXERCISELIST_H
+
+#include <wx/wx.h>
+#include <wx/listctrl.h>
+
+class TimedExerciseList : public wxListView
+{
+
+};
+
+#endif // !TIMEDEXERCISELIST_H
