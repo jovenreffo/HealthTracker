5344a46ae2dc179a6d0b123e91ae1c027f52ed5b
0x413 <jovenreffo@gmail.com>
2023-05-11 07:25:22 -0600

    - Class members + funcs

--- H/src/TimedExerciseList.h
+++ H/src/TimedExerciseList.h
@@ -4,9 +4,34 @@
 #include <wx/wx.h>
 #include <wx/listctrl.h>
 
+struct TEL
+{
+	int m_hours;
+	int m_mins;
+	int m_secs;
+	int m_cals;
+	double m_distance;
+};
+
 class TimedExerciseList : public wxListView
 {
+private:
+	TEL m_exerciseInfo;
 
+	wxMenu* m_pMenu;
+	wxImageList* m_pImageList;
+
+public:
+	TimedExerciseList(wxWindow* parent,
+		wxWindowID id,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = wxLC_REPORT | wxLC_SINGLE_SEL);
+
+	void Init();
+	void SetupMenu();
+	void SetupColumns();
+	void SetupImageList();
 };
 
 #endif // !TIMEDEXERCISELIST_H
