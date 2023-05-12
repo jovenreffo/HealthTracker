d1e0034294216d5f07c15a10633b99938a6161ca
0x413 <jovenreffo@gmail.com>
2023-05-12 06:58:46 -0600

    `TEL` dev

--- H/src/TimedExerciseList.cc
+++ H/src/TimedExerciseList.cc
@@ -1,4 +1,5 @@
 #include "TimedExerciseList.h"
+#include "StandardPath.hpp"
 
 TimedExerciseList::TimedExerciseList(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxListView(parent, id, pos, size, style)
@@ -20,10 +21,17 @@ void TimedExerciseList::SetupMenu()
 
 void TimedExerciseList::SetupColumns()
 {
-
+	this->AppendColumn(_T("Duration"));
+	this->AppendColumn(_T("Distance (km)"));
+	this->AppendColumn(_T("Calories"));
 }
 
 void TimedExerciseList::SetupImageList()
 {
+	m_pImageList = new wxImageList(16, 16);
 
+	m_calorieBmp = wxBitmap(path_data::dataDir + _T("\\images\\calorie_small.bmp"));
+	m_pImageList->Add(m_calorieBmp);
+
+	this->AssignImageList(m_pImageList, wxIMAGE_LIST_SMALL);
 }
