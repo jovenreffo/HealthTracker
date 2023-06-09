d6bd5a2f33e4b44d8ef3a4e70d37ee51a941da53
0x413 <jovenreffo@gmail.com>
2023-06-09 07:00:00 -0600

    - do some initialization

--- H/src/SpreadsheetWindow.cc
+++ H/src/SpreadsheetWindow.cc
@@ -31,6 +31,12 @@ void SpreadsheetWindow::Init()
 	this->SetupSizers();
 }
 
+void SpreadsheetWindow::SetupMenu()
+{
+	m_pFileMenu = new wxMenu();
+	m_pMenuBar = new wxMenuBar();
+}
+
 void SpreadsheetWindow::SetupSizing()
 {
 
@@ -38,10 +44,14 @@ void SpreadsheetWindow::SetupSizing()
 
 void SpreadsheetWindow::SetupControls()
 {
-
+	m_pGrid = new wxGrid(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+	m_pGrid->CreateGrid(100, 10); // Create a grid with 100 rows and 10 columns
 }
 
 void SpreadsheetWindow::SetupSizers()
 {
+	m_pTopSizer = new wxBoxSizer(wxVERTICAL);
+	this->SetSizerAndFit(m_pTopSizer);
 
+	m_pTopSizer->Add(m_pGrid, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
 }
