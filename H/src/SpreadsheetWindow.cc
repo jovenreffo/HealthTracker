32b5059d26754b52b19a07dc3c8809ec738e8585
0x413 <jovenreffo@gmail.com>
2023-06-12 07:02:52 -0600

    `ExercisePanel` dev

--- H/src/SpreadsheetWindow.cc
+++ H/src/SpreadsheetWindow.cc
@@ -37,7 +37,6 @@ void SpreadsheetWindow::Init()
 	this->SetupSizers();
 	this->SetupMenu();
 	this->SetupSizing();
-	this->SetupConfig();
 
 	this->BindEvents();
 }
@@ -45,12 +44,19 @@ void SpreadsheetWindow::Init()
 void SpreadsheetWindow::SetupMenu()
 {
 	m_pFileMenu = new wxMenu();
+	m_pInsertMenu = new wxMenu();
 	m_pMenuBar = new wxMenuBar();
 
+	// File menu
 	m_pFileMenu->AppendSeparator();
 	m_pFileMenu->Append(wxID_EXIT, _T("&Exit"));
 
+	// Insert menu
+
+
+	// Menubar
 	m_pMenuBar->Append(m_pFileMenu, _T("&File"));
+	m_pMenuBar->Append(m_pInsertMenu, _T("&Insert"));
 	this->SetMenuBar(m_pMenuBar);
 }
 
@@ -62,8 +68,7 @@ void SpreadsheetWindow::SetupSizing()
 
 void SpreadsheetWindow::SetupControls()
 {
-	m_pGrid = new wxGrid(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
-	m_pGrid->CreateGrid(100, 10); // Create a grid with 100 rows and 10 columns
+	m_pGrid = new ExerciseGrid(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
 }
 
 void SpreadsheetWindow::SetupSizers()
@@ -72,33 +77,6 @@ void SpreadsheetWindow::SetupSizers()
 	this->SetSizerAndFit(m_pTopSizer);
 
 	m_pTopSizer->Add(m_pGrid, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
-}
-
-void SpreadsheetWindow::SetupConfig()
-{
-	wxConfigBase* pConfig = wxConfigBase::Get();
-	if (pConfig == nullptr)
-		return;
-
-	pConfig->SetPath(_T("/Preferences/"));
-
-	if (pConfig->Read(_T("CheckFont"), 0L) == 1L)
-	{
-		for (auto rows{ 0 }; rows < m_pGrid->GetNumberRows(); ++rows)
-		{
-			for (auto cols{ 0 }; cols < m_pGrid->GetNumberCols(); ++cols)
-			{
-				m_pGrid->SetCellFont(rows, cols, wxFont(
-					pConfig->Read("FontSize", 10L),
-					static_cast<wxFontFamily>(pConfig->Read("FontFamily", static_cast<long>(wxFONTFAMILY_DEFAULT))),
-					static_cast<wxFontStyle>(pConfig->Read("FontStyle", static_cast<long>(wxFONTSTYLE_NORMAL))),
-					wxFONTWEIGHT_NORMAL,
-					pConfig->Read("FontUnderline", 0L),
-					pConfig->Read("FaceName", "")
-				));
-			}
-		}
-	}
 }
 
 void SpreadsheetWindow::BindEvents()
@@ -113,4 +91,46 @@ void SpreadsheetWindow::OnExit(wxCommandEvent& event)
 {
 	if (wxMessageBox(_T("Are you sure you want to exit?"), _T("Confirm"), wxYES_NO | wxICON_WARNING) == wxYES)
 		this->Close(true);
+}
+
+// ===== ExerciseGrid ======
+
+ExerciseGrid::ExerciseGrid(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+	: wxGrid(parent, id, pos, size, style)
+{
+	this->CreateGrid(100, 10); // Create a grid with 100 rows and 10 columns
+
+	this->Init();
+}
+
+void ExerciseGrid::Init()
+{
+	this->SetupConfig();
+}
+
+void ExerciseGrid::SetupConfig()
+{
+	wxConfigBase* pConfig = wxConfigBase::Get();
+	if (pConfig == nullptr)
+		return;
+
+	pConfig->SetPath(_T("/Preferences/"));
+
+	if (pConfig->Read(_T("CheckFont"), 0L) == 1L)
+	{
+		for (auto rows{ 0 }; rows < this->GetNumberRows(); ++rows)
+		{
+			for (auto cols{ 0 }; cols < this->GetNumberCols(); ++cols)
+			{
+				this->SetCellFont(rows, cols, wxFont(
+					pConfig->Read("FontSize", 10L),
+					static_cast<wxFontFamily>(pConfig->Read("FontFamily", static_cast<long>(wxFONTFAMILY_DEFAULT))),
+					static_cast<wxFontStyle>(pConfig->Read("FontStyle", static_cast<long>(wxFONTSTYLE_NORMAL))),
+					wxFONTWEIGHT_NORMAL,
+					pConfig->Read("FontUnderline", 0L),
+					pConfig->Read("FaceName", "")
+				));
+			}
+		}
+	}
 }
