c7611f1a584c5f740921bd95c26e160d09b35f0b
0x413 <jovenreffo@gmail.com>
2023-07-30 08:18:24 -0600

    - `NutrientGoal` class

--- H/src/Nutrient Total/Total.hpp
+++ H/src/Nutrient Total/Total.hpp
@@ -3,10 +3,18 @@
 
 struct Total
 {
+	// Members
 	int m_calTotal;
 	int m_carbTotal;
 	int m_proteinTotal;
 	int m_fiberTotal;
+
+	// Getters
+	const int GetCalTotal() const { return m_calTotal; }
+	const int GetCarbTotal() const { return m_carbTotal; }
+	const int GetProteinTotal() const { return m_proteinTotal; }
+	const int GetFibreTotal() const { return m_fiberTotal; }
+
 
 	void ResetTotal()
 	{
@@ -51,6 +59,32 @@ public:
 	const int GetProtein() const { return m_protein; }
 	const int GetCarbohydrates() const { return m_carbohydrates; }
 	const int GetFiber() const { return m_fiber; }
+};
+
+class NutrientGoals
+{
+private:
+	int m_calorieGoal;
+	int m_proteinGoal;
+
+	bool m_bActive;
+
+public:
+	NutrientGoals() = default;
+	NutrientGoals(int calorieGoal, int proteinGoal)
+		: m_calorieGoal{ calorieGoal }, m_proteinGoal{ proteinGoal }
+	{
+		m_bActive = true;
+	}
+
+	bool IsActive()
+	{
+		if (m_bActive) return true;
+		else return false;
+	}
+
+	const int GetCalorieGoal() const { return m_calorieGoal; }
+	const int GetProteinGoal() const { return m_proteinGoal; }
 };
 
 #endif
