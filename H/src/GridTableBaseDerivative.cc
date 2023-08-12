1960d5c873b275a948d8ca3c9f87fa3f2b69a1e7
0x413 <jovenreffo@gmail.com>
2023-08-12 10:18:16 -0600

    .

--- H/src/GridTableBaseDerivative.cc
+++ H/src/GridTableBaseDerivative.cc
@@ -5,3 +5,28 @@ GridTableBaseDerivative::GridTableBaseDerivative(wxGrid* pGrid)
 {
 
 }
+
+int GridTableBaseDerivative::GetNumberRows()
+{
+	return m_pGrid->GetNumberRows();
+}
+
+int GridTableBaseDerivative::GetNumberCols()
+{
+	return m_pGrid->GetNumberCols();
+}
+
+bool GridTableBaseDerivative::IsEmptyCell(int r, int c)
+{
+	return m_pGrid->GetCellValue(r, c).IsEmpty();
+}
+
+wxString GridTableBaseDerivative::GetValue(int r, int c)
+{
+	return m_pGrid->GetCellValue(r, c);
+}
+
+void GridTableBaseDerivative::SetValue(int r, int c, const wxString& value)
+{
+	m_pGrid->SetCellValue(r, c, value);
+}
