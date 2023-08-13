5ecc481a55726378ff56276dc6f5621f049c6231
0x413 <jovenreffo@gmail.com>
2023-08-13 08:15:05 -0600

    - add members and funcs

--- H/src/GridCommand.h
+++ H/src/GridCommand.h
@@ -1,9 +1,19 @@
 #pragma once
 
 #include <wx/cmdproc.h>
+#include <wx/grid.h>
 #include <wx/wx.h>
 
 class GridCommand: public wxCommand
 {
+private:
+	wxGrid* m_pGrid;
 
+public:
+	GridCommand(wxGrid* pGrid, bool bCanUndo, const wxString& name);
+	
+	// Overload functions
+	bool CanUndo() const override;
+	wxString GetName() const override;
+	bool Undo() override;
 };
