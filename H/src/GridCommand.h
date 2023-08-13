a432c6550d64600bef2d0377761916517434866c
0x413 <jovenreffo@gmail.com>
2023-08-13 08:47:07 -0600

    - Getters for `GridCellState`

--- H/src/GridCommand.h
+++ H/src/GridCommand.h
@@ -4,6 +4,29 @@
 #include <wx/grid.h>
 #include <wx/wx.h>
 
+// store the current 
+class GridCellState
+{
+private:
+	wxString m_cellValue;
+	wxFont m_cellFont;
+	wxSize m_cellSize; // number of rows & cols associated with the cell
+	wxColour m_cellBackgroundColour;
+
+public:
+	GridCellState(const wxString& cellValue,
+		const wxString& cellFont,
+		const wxSize& cellSize,
+		const wxColour& colour);
+
+	// Getters
+	const wxString& GetCellValue() const { return m_cellValue; }
+	const wxFont& GetCellFont() const { return m_cellFont; }
+	const wxSize& GetCellSize() const { return m_cellSize; }
+	const wxColour& GetCellColour() const{ return m_cellBackgroundColour; }
+};
+
+// class for taking action on the grid for undo/redo
 class GridCommand: public wxCommand
 {
 private:
