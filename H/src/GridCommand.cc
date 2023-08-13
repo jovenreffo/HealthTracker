64c32344ebe25660ef9cffc4ea8efc0c6c329613
0x413 <jovenreffo@gmail.com>
2023-08-13 08:47:14 -0600

    - ctor for `GridCellState`

--- H/src/GridCommand.cc
+++ H/src/GridCommand.cc
@@ -1,8 +1,19 @@
 #include "GridCommand.h"
 
+// ===== GridCellState =====
+
+GridCellState::GridCellState(const wxString& cellValue, const wxString& cellFont, const wxSize& cellSize, const wxColour& colour)
+	: m_cellValue{ cellValue }, m_cellFont{ cellFont }, m_cellSize{ cellSize }, m_cellBackgroundColour{ colour }
+{
+
+}
+
+// ===== GridCommand =====
+
 GridCommand::GridCommand(wxGrid* pGrid, bool bCanUndo, const wxString& name)
 	: wxCommand(bCanUndo, name), m_pGrid{ pGrid }
 {
+
 }
 
 bool GridCommand::CanUndo() const
