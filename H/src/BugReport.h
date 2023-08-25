9ace23a574612337a5a8e6c0d51bcf06710bed61
0x413 <jovenreffo@gmail.com>
2023-08-25 09:31:59 -0600

    - setup members

--- H/src/BugReport.h
+++ H/src/BugReport.h
@@ -4,10 +4,26 @@
 #include <wx/dialog.h>
 #include <wx/textctrl.h>
 #include <wx/bmpbuttn.h>
+#include <wx/sizer.h>
 #include "PrefsDlg.h"
 
 class BugReport : public wxDialog
 {
+private:
+	// Controls
+	wxBitmapButton* m_pModifyName;
+	wxTextCtrl* m_pNameTxt;
+	wxTextCtrl* m_pEmailTxt;
+
+	// Sizers
+	wxBoxSizer* m_pTopSizer;
+	wxBoxSizer* m_pButtonSizer;
+	wxFlexGridSizer* m_pControlSizer;
+
+	// Values
+	wxString m_name;
+	wxString m_email;
+
 public:
 	BugReport(wxWindow* parent,
 		wxWindowID id,
@@ -16,6 +32,18 @@ public:
 		const wxSize& size = wxDefaultSize,
 		long style = wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxCLOSE_BOX);
 	~BugReport();
+
+	// Setup for dialog
+	void Init();
+	void LoadConfig();
+	void SetupControls();
+	void SetupSizers();
+	void SetupSizing();
+
+	// Events for buttons
+	void OnModify(wxCommandEvent& event);
+	void OnOK(wxCommandEvent& event);
+	void OnCancel(wxCommandEvent& event);
 };
 
 #endif
