fa2e294a99f4f98ab1346464b320a8d0160ba1f3
0x413 <jovenreffo@gmail.com>
2023-09-10 15:39:33 -0600

    `ClientPanel.cc`

---
+++ H/src/ClientPanel.cc
@@ -0,0 +1,6 @@
+#include "ClientPanel.h"
+
+ClientPanel::ClientPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+	: wxPanel(parent, id, pos, size, style)
+{
+}
