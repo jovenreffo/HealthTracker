2f44f300e383727053f82eb680cd559f7b27defd
0x413 <jovenreffo@gmail.com>
2023-09-11 07:04:01 -0600

    - `ClientList` class

--- H/src/ClientPanel.h
+++ H/src/ClientPanel.h
@@ -1,17 +1,41 @@
 #ifndef CLIENTPANEL_H
 #define CLIENTPANEL_H
 
-#include <wx/panel.h>
 #include <wx/wx.h>
+#include <wx/font.h>
+#include <wx/panel.h>
+#include <wx/listctrl.h>
+
+class ClientList : public wxListView
+{
+public:
+	ClientList(wxWindow* parent,
+		wxWindowID id,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = wxLC_REPORT | wxLC_EDIT_LABELS);
+};
 
 class ClientPanel : public wxPanel
 {
+private:
+	wxButton* m_pAddClientBtn;
+	ClientList* m_pClientList;
+
 public:
 	ClientPanel(wxWindow* parent,
 		wxWindowID id,
 		const wxPoint& pos = wxDefaultPosition,
 		const wxSize& size = wxDefaultSize,
 		long style = 0L);
+	~ClientPanel();
+
+	// Panel setup
+	void InitClientPanel();
+	void SetupMemberControls();
+	void SetupSizers();
+
+	// Events
 };
 
 #endif
