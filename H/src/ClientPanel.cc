6ec7317e2a4dce73d5a7ef6757efe01cf2254098
0x413 <jovenreffo@gmail.com>
2023-09-11 07:04:06 -0600

    - general setup

--- H/src/ClientPanel.cc
+++ H/src/ClientPanel.cc
@@ -1,6 +1,37 @@
 #include "ClientPanel.h"
 
+// === ClientList === 
+
+ClientList::ClientList(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+	: wxListView(parent, id, pos, size, style)
+{
+}
+
+// === ClientPanel === 
+
 ClientPanel::ClientPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxPanel(parent, id, pos, size, style)
+{
+	this->InitClientPanel();
+
+	// Bind events
+}
+
+ClientPanel::~ClientPanel()
+{
+	// Unbind events
+}
+
+void ClientPanel::InitClientPanel()
+{
+	this->SetupMemberControls();
+	this->SetupSizers();
+}
+
+void ClientPanel::SetupMemberControls()
+{
+}
+
+void ClientPanel::SetupSizers()
 {
 }
