77e57ce2a20355537f018dbb4a7c830e59b096ef
0x413 <jovenreffo@gmail.com>
2023-09-11 07:05:19 -0600

    - add row/col members - overload `operator=`

--- H/src/GridCommand.h
+++ H/src/GridCommand.h
@@ -12,19 +12,46 @@ private:
 	wxFont m_cellFont;
 	wxSize m_cellSize; // number of rows & cols associated with the cell
 	wxColour m_cellBackgroundColour;
+	int m_row;
+	int m_col;
+
+	bool m_bActive{ false };
 
 public:
 	GridCellState() = default;
-	GridCellState(const wxString& cellValue,
-		const wxString& cellFont,
+	GridCellState(int row,
+		int col,
+		const wxString& cellValue,
+		const wxFont& cellFont,
 		const wxSize& cellSize,
 		const wxColour& colour);
 
 	// Getters
+	bool IsActive() const { return m_bActive; }
+
+	const int GetRow() const { return m_row; }
+	const int GetCol() const { return m_col; }
+
 	const wxString& GetCellValue() const { return m_cellValue; }
 	const wxFont& GetCellFont() const { return m_cellFont; }
 	const wxSize& GetCellSize() const { return m_cellSize; }
 	const wxColour& GetCellColour() const{ return m_cellBackgroundColour; }
+
+	// overload operator=
+	GridCellState& operator=(const GridCellState& other)
+	{
+		// Protect against self assignment
+		if (this == &other)
+			return *this;
+
+		// Copy the data
+		this->m_row = other.m_row;
+		this->m_col = other.m_col;
+		this->m_cellValue = other.m_cellValue;
+		this->m_cellFont = other.m_cellFont;
+		this->m_cellSize = other.m_cellSize;
+		this->m_cellBackgroundColour = other.m_cellBackgroundColour;
+	}
 };
 
 // class for taking action on the grid for undo/redo
