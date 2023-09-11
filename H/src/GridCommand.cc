4249ed1c1977769c93fc4b226a0c6c19f9f6e77e
0x413 <jovenreffo@gmail.com>
2023-09-11 07:05:02 -0600

    - row/col member

--- H/src/GridCommand.cc
+++ H/src/GridCommand.cc
@@ -2,10 +2,11 @@
 
 // ===== GridCellState =====
 
-GridCellState::GridCellState(const wxString& cellValue, const wxString& cellFont, const wxSize& cellSize, const wxColour& colour)
-	: m_cellValue{ cellValue }, m_cellFont{ cellFont }, m_cellSize{ cellSize }, m_cellBackgroundColour{ colour }
+GridCellState::GridCellState(int row, int col, const wxString& cellValue, const wxFont& cellFont, const wxSize& cellSize, const wxColour& colour)
+	: m_row{ row }, m_col{ col }, m_cellValue{ cellValue }, m_cellFont{ cellFont }, m_cellSize{ cellSize }, m_cellBackgroundColour{ colour }
 {
-
+	// this signifies an instance of this class has been initiated, use this bool to check if it is active
+	m_bActive = true;
 }
 
 // ===== GridCommand =====
