8869a2ace1c3259fd3e4f58c72820cd3c561af0d
0x413 <jovenreffo@gmail.com>
2023-09-15 07:17:49 -0600

    - list/panel setup

--- H/src/ClientPanel.h
+++ H/src/ClientPanel.h
@@ -2,25 +2,51 @@
 #define CLIENTPANEL_H
 
 #include <wx/wx.h>
+#include <wx/menu.h>
 #include <wx/font.h>
 #include <wx/panel.h>
 #include <wx/listctrl.h>
 
 class ClientList : public wxListView
 {
+private:
+	wxFont m_font; // The font used for items in the list will be bigger, and slightly bolded.
+
+	// Controls
+	wxMenu* m_pMenu;
+	wxImageList* m_pImageList;
+
+	// Bitmaps, icons, images
+	wxBitmap m_clientIconBmp;
+
 public:
 	ClientList(wxWindow* parent,
 		wxWindowID id,
 		const wxPoint& pos = wxDefaultPosition,
 		const wxSize& size = wxDefaultSize,
 		long style = wxLC_REPORT | wxLC_EDIT_LABELS);
+
+	// Setup
+	void SetupList();
+	void SetupColumn();
+	void SetupImageList();
+	void SetupMenu();
+	void SetListFont();
+	
+	// Actions & events
+	void AddItem(const wxString& name);
 };
 
 class ClientPanel : public wxPanel
 {
 private:
+	// Controls
 	wxButton* m_pAddClientBtn;
 	ClientList* m_pClientList;
+
+	// Sizers
+	wxBoxSizer* m_pTopSizer;
+	wxBoxSizer* m_pTopButtonSizer;
 
 public:
 	ClientPanel(wxWindow* parent,
