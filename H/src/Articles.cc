8aef4f01d0033d8ad270e528ead600f817c28084
jovenreffo <jovenreffo@gmail.com>
2023-10-15 17:22:14 -0600

    - do some more general setup for the article tab

--- H/src/Articles.cc
+++ H/src/Articles.cc
@@ -1,12 +1,61 @@
 #include "Articles.h"
 
+
+ArticleHtmlPanel::ArticleHtmlPanel(const wxString& htmlFilePath, wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+	: wxPanel(parent, id, pos, size, style), m_filePath{ htmlFilePath }
+{
+}
+
+ArticleHtmlPanel::~ArticleHtmlPanel()
+{
+}
+
+// ArticlePanel
+
 ArticlePanel::ArticlePanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxPanel(parent, id, pos, size, style)
 {
-
+	this->SetupArticlePanel();
 }
 
 ArticlePanel::~ArticlePanel()
 {
 
+}
+
+void ArticlePanel::SetupArticlePanel()
+{
+	this->SetupSizers();
+}
+
+void ArticlePanel::SetupFitnessArticlePanel()
+{
+}
+
+void ArticlePanel::SetupFocusArticlePanel()
+{
+}
+
+void ArticlePanel::SetupNutritionArticlePanel()
+{
+}
+
+void ArticlePanel::SetupSizers()
+{
+	// Initialize the top sizer
+	m_pTopSizer = new wxFlexGridSizer(2, wxSize(5, 1));
+	this->SetSizerAndFit(m_pTopSizer);
+
+	// Sizers that will be 'containers' for articles
+	m_pFitnessSizer = new wxStaticBoxSizer(wxVERTICAL, this, _T("Fitness"));
+	m_pFocusSizer = new wxStaticBoxSizer(wxVERTICAL, this, _T("Productivity && Focus")); // use double ampersands because wxwidgets treats a single one as an accelerator
+	m_pNutritionSizer = new wxStaticBoxSizer(wxVERTICAL, this, _T("Food && Nutrition"));
+
+	m_pTopSizer->Add(m_pFitnessSizer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+	m_pTopSizer->Add(m_pFocusSizer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+	m_pTopSizer->Add(m_pNutritionSizer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+}
+
+void ArticlePanel::AddArticle(const wxBitmap& bmp, const wxString& link, const wxString& articleTitle)
+{
 }
--- H/src/Articles.h
+++ H/src/Articles.h
@@ -3,6 +3,7 @@
 
 #include <wx/wx.h>
 #include <wx/panel.h>
+#include <wx/sizer.h>
 
 class ArticleHtmlPanel : public wxPanel
 {
@@ -21,6 +22,17 @@ public:
 
 class ArticlePanel : public wxPanel
 {
+private:
+	// sizers for different article sections
+	wxFlexGridSizer* m_pTopSizer;
+	wxStaticBoxSizer* m_pFitnessSizer;
+	wxStaticBoxSizer* m_pFocusSizer; // productivity & focus
+	wxStaticBoxSizer* m_pNutritionSizer;
+
+
+	// the static box from the sizers to be used as parents
+
+
 public:
 	ArticlePanel(wxWindow* parent,
 		wxWindowID id,
@@ -31,6 +43,11 @@ public:
 
 	// setup
 	void SetupArticlePanel();
+
+	void SetupFitnessArticlePanel();
+	void SetupFocusArticlePanel();
+	void SetupNutritionArticlePanel();
+	void SetupSizers();
 
 	// actions
 	void AddArticle(const wxBitmap& bmp, const wxString& link, const wxString& articleTitle);
