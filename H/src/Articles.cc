926e5db0e3430ddf59c9e72aa219a58a586c39b6
jovenreffo <jovenreffo@gmail.com>
2023-10-23 06:57:55 -0600

    - general initialization of members

--- H/src/Articles.cc
+++ H/src/Articles.cc
@@ -1,12 +1,35 @@
 #include "Articles.h"
+#include "StandardPath.hpp"
 
 
 ArticleHtmlPanel::ArticleHtmlPanel(const wxString& htmlFilePath, wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxPanel(parent, id, pos, size, style), m_filePath{ htmlFilePath }
 {
+	// make the background colour a little bit darker than white to be distinct
+	this->SetBackgroundColour(wxColour(240, 240, 240));
+
+	m_pHtmlWindow = new wxHtmlWindow(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxHW_DEFAULT_STYLE);
+	m_pHtmlWindow->SetBorders(0);
+	m_pHtmlWindow->LoadPage(m_filePath);
+	m_pHtmlWindow->SetInitialSize(wxSize(m_pHtmlWindow->GetInternalRepresentation()->GetWidth(), m_pHtmlWindow->GetInternalRepresentation()->GetHeight()));
+
+	// Add the HTML page to the sizer
+	this->AddPageToSizer();
 }
 
 ArticleHtmlPanel::~ArticleHtmlPanel()
+{
+}
+
+void ArticleHtmlPanel::AddPageToSizer()
+{
+	m_pTopSizer = new wxBoxSizer(wxVERTICAL);
+	this->SetSizerAndFit(m_pTopSizer);
+
+	m_pTopSizer->Add(m_pHtmlWindow, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+}
+
+void ArticleHtmlPanel::AddArticle(const wxBitmap& bmp, const wxString& link, const wxString& articleTitle)
 {
 }
 
@@ -25,6 +48,11 @@ ArticlePanel::~ArticlePanel()
 
 void ArticlePanel::SetupArticlePanel()
 {
+	// setup individual HTML windows
+	this->SetupFitnessArticlePanel();
+	this->SetupFocusArticlePanel();
+	this->SetupNutritionArticlePanel();
+
 	this->SetupSizers();
 }
 
@@ -54,8 +82,4 @@ void ArticlePanel::SetupSizers()
 	m_pTopSizer->Add(m_pFitnessSizer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
 	m_pTopSizer->Add(m_pFocusSizer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
 	m_pTopSizer->Add(m_pNutritionSizer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
-}
-
-void ArticlePanel::AddArticle(const wxBitmap& bmp, const wxString& link, const wxString& articleTitle)
-{
 }
