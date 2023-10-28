0bfcaf60a9ddcd564b4f162dc99258a1c25fa4a3
jovenreffo <jovenreffo@gmail.com>
2023-10-28 09:54:38 -0600

    - Load entries on startup

--- H/src/WPLists.cc
+++ H/src/WPLists.cc
@@ -1,4 +1,5 @@
 #include <wx/msgdlg.h>
+#include <wx/config.h>
 #include "WPLists.h"
 #include "StandardPath.hpp"
 #include "WorkoutWindow.h" // A circular dependency will occur if this is put in WPLists.h
@@ -27,11 +28,77 @@ WorkoutList::WorkoutList(wxWindow* parent, wxWindowID id, const wxPoint& pos, co
 	this->Init();
 }
 
+WorkoutList::~WorkoutList()
+{
+	this->SaveToConfig();
+}
+
 void WorkoutList::Init()
 {
 	this->SetupImageList();
 	this->SetupColumn();
 	this->SetupMenu();
+
+	this->LoadFromConfig();
+}
+
+void WorkoutList::LoadFromConfig()
+{
+	wxConfigBase* pConfig = wxConfigBase::Get();
+	if (pConfig == nullptr)
+		return;
+
+	pConfig->SetPath(_T("/WPLists/"));
+	m_numEntries = pConfig->Read(_T("NumEntries"), 0L);
+	
+	// Load entry content before names so the items can be added
+	wxArrayString entryContentArr;
+	pConfig->SetPath(_T("/WPLists/WorkoutContent"));
+	for (auto i{ m_numEntries - 1 }; i >= 0; --i) // reverse order because of how the entries are saved to the config file
+	{
+		wxString curr;
+
+		if (pConfig->Read(std::to_string(i), &curr))
+			entryContentArr.Add(curr);
+	}
+
+	// load the entry names and add items to the list
+	pConfig->SetPath(_T("/WPLists/WorkoutNames"));
+
+	for (auto i{ m_numEntries - 1 }; i >= 0; --i)
+	{
+		wxString curr;
+
+		if (pConfig->Read(std::to_string(i), &curr))
+			this->AddItem(curr, entryContentArr[i]);
+	}
+}
+
+void WorkoutList::SaveToConfig()
+{
+	wxConfigBase* pConfig = wxConfigBase::Get();
+	if (pConfig == nullptr)
+		return;
+
+	pConfig->SetPath(_T("/WPLists/"));
+	// save the number of entries
+	m_numEntries = this->GetItemCount(); // read the number of entries into the variable
+	pConfig->Write(_T("NumEntries"), m_numEntries); // then save it to the config
+	
+	// save workout names
+	pConfig->SetPath(_T("/WPLists/WorkoutNames"));
+	for (auto i{ 0 }; i < this->GetItemCount(); ++i)
+	{
+		pConfig->Write(std::to_string(i), this->GetItemText(i));
+	}
+
+	// save workout content
+	pConfig->SetPath(_T("/WPLists/WorkoutContent"));
+	for (auto i{ 0 }; i < this->GetItemCount(); ++i)
+	{
+		// Recieve content associated at index 'i'
+		pConfig->Write(std::to_string(i), this->GetContent()[i].GetContent());
+	}
 }
 
 void WorkoutList::SetupImageList()
