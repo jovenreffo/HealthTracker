9e6d51f9442cdc31d5aa3f0bc11bd1cf35471069
jovenreffo <jovenreffo@gmail.com>
2023-11-14 09:17:08 -0700

    - add members and functions for new `ArticleContainer` class

--- H/src/Articles.h
+++ H/src/Articles.h
@@ -5,10 +5,19 @@
 #include <wx/panel.h>
 #include <wx/sizer.h>
 #include <wx/listctrl.h>
-#include <wx/html/htmlwin.h>
+#include <wx/clipbrd.h>
 
 class ArticleContainer : public wxPanel
 {
+private:
+	// a list view and a string array for holding the links of each article
+	// when an item from m_pArticleList is double clicked or Right click -> Open, the prgram will launch the default browser and pass in the link
+	wxListView* m_pArticleList;
+	wxArrayString m_articleLinkArr;
+
+	// wxMenu for when m_pArticleList is right clicked and actions must be displayed
+	wxMenu* m_pMenu;
+
 public:
 	ArticleContainer(wxWindow* parent,
 		wxWindowID id,
@@ -16,7 +25,19 @@ public:
 		const wxSize& size = wxDefaultSize,
 		long style = 0L);
 	~ArticleContainer();
+
+	// Member and general setup for article list
+	void InitializeList();
+	void SetupMenu();
+	void AddArticleItem(const wxString& articleLink, const wxString& articleTitle, const wxBitmap& articleThumbnail);
+
+	// Events
+	void OnRightClickItem(wxListEvent& event);
+	void OnOpenArticle(wxCommandEvent& event);
+	void OnCopyArticleLink(wxCommandEvent& event);
 };
+
+// === ArticlePanel ===
 
 class ArticlePanel : public wxPanel
 {
