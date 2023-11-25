f8e65d78099a53e544d5173a947237b9078e14f8
jovenreffo <jovenreffo@gmail.com>
2023-11-25 08:33:51 -0700

    - do some additions and removals, setup

--- H/src/Articles.cc
+++ H/src/Articles.cc
@@ -1,15 +1,75 @@
 #include "Articles.h"
 #include "StandardPath.hpp"
 
+// ArticleList
+
+ArticleList::ArticleList(const wxString& which, wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+	: wxListView(parent, id, pos, size, style), m_listTitle{ which }
+{
+	this->InitializeArticleList();
+
+	// Bind events
+	this->Bind(wxEVT_LIST_ITEM_RIGHT_CLICK, &ArticleList::OnRightClickItem, this);
+	m_pMenu->Bind(wxEVT_MENU, &ArticleList::OnOpenLinkInBrowser, this, wxID_OPEN);
+}
+
+ArticleList::~ArticleList()
+{
+	// unbind
+
+}
+
+void ArticleList::InitializeArticleList()
+{
+	this->SetupMenu();
+	this->SetupList();
+	this->SetupSizer();
+}
+
+void ArticleList::SetupMenu()
+{
+	m_pMenu = new wxMenu();
+	m_pMenu->Append(wxID_OPEN, _T("Open in browser"));
+}
+
+void ArticleList::SetupList()
+{
+	this->AppendColumn(m_listTitle);
+}
+
+void ArticleList::SetupSizer()
+{
+}
+
+void ArticleList::AddArticleItem(const wxString& articleLink, const wxString& articleTitle, const wxBitmap& articleThumbnail)
+{
+}
+
+void ArticleList::OnRightClickItem(wxListEvent& event)
+{
+}
+
+void ArticleList::OnOpenLinkInBrowser(wxCommandEvent& event)
+{
+}
+
 // ArticleContainer
 
 ArticleContainer::ArticleContainer(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 	: wxPanel(parent, id, pos, size, style)
 {
+	this->InitializeList();
+
+	this->SetBackgroundColour(*wxWHITE);
 }
 
 ArticleContainer::~ArticleContainer()
 {
+}
+
+void ArticleContainer::InitializeList()
+{
+	ArticleList* m_pArticleList;
 }
 
 
@@ -29,23 +89,33 @@ ArticlePanel::~ArticlePanel()
 void ArticlePanel::SetupArticlePanel()
 {
 	// setup individual HTML windows
-	this->SetupFitnessArticlePanel();
-	this->SetupFocusArticlePanel();
-	this->SetupNutritionArticlePanel();
+	this->SetupContainers();
 
 	this->SetupSizers();
 }
 
-void ArticlePanel::SetupFitnessArticlePanel()
+void ArticlePanel::SetupContainers()
 {
+	this->SetupFitnessContainer();
+	this->SetupFocusContainer();
+	this->SetupNutritionContainer();
 }
 
-void ArticlePanel::SetupFocusArticlePanel()
+void ArticlePanel::SetupFitnessContainer()
 {
+	m_pFitnessContainer = new ArticleContainer(this, wxID_ANY);
 }
 
-void ArticlePanel::SetupNutritionArticlePanel()
+void ArticlePanel::SetupFocusContainer()
 {
+	m_pFocusContainer = new ArticleContainer(this, wxID_ANY);
+
+}
+
+void ArticlePanel::SetupNutritionContainer()
+{
+	m_pNutritionContainer = new ArticleContainer(this, wxID_ANY);
+
 }
 
 void ArticlePanel::SetupSizers()
@@ -59,6 +129,13 @@ void ArticlePanel::SetupSizers()
 	m_pFocusSizer = new wxStaticBoxSizer(wxVERTICAL, this, _T("Productivity && Focus")); // use double ampersands because wxwidgets treats a single one as an accelerator
 	m_pNutritionSizer = new wxStaticBoxSizer(wxVERTICAL, this, _T("Food && Nutrition"));
 
+	// add containers to sizers
+	m_pFitnessSizer->Add(m_pFitnessContainer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+	m_pFocusSizer->Add(m_pFocusContainer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+	m_pNutritionSizer->Add(m_pNutritionContainer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
+
+
+	// add all sizers to the main sizer
 	m_pTopSizer->Add(m_pFitnessSizer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
 	m_pTopSizer->Add(m_pFocusSizer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
 	m_pTopSizer->Add(m_pNutritionSizer, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
