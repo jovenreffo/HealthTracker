4d1de36b6d718a7e29716144625b4993ded769ba
jovenreffo <jovenreffo@gmail.com>
2023-12-23 09:15:24 -0700

    - Fix problems with the `ArticleNotebook`

--- H/src/Articles.cc
+++ H/src/Articles.cc
@@ -1,5 +1,6 @@
 #include "Articles.h"
 #include "StandardPath.hpp"
+#include <wx/textctrl.h>
 
 // add  preprocessor definition for the path so it doesn't have to be typed out
 #define ARTICLE_IMG_PATH (path_data::dataDir + _T("\\Images\\Articles\\"))
@@ -60,45 +61,6 @@ void ArticleList::OnOpenLinkInBrowser(wxCommandEvent& event)
 {
 }
 
-// ArticleNotebook
-
-ArticleNotebook::ArticleNotebook(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
-{
-	this->Init();
-}
-
-ArticleNotebook::~ArticleNotebook()
-{
-	this->DestroyChildren();
-}
-
-void ArticleNotebook::Init()
-{
-	this->SetupControls();
-	this->SetupPanels();
-	this->SetupSizer();
-	this->SetupNotebook();
-}
-
-void ArticleNotebook::SetupControls()
-{
-}
-
-void ArticleNotebook::SetupPanels()
-{
-}
-
-void ArticleNotebook::SetupSizer()
-{
-}
-
-void ArticleNotebook::SetupNotebook()
-{
-	m_pImageList = new wxImageList(26, 26);
-
-	this->AssignImageList(m_pImageList);
-}
-
 // ArticleContainer
 
 ArticleContainer::ArticleContainer(const wxString& which, wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
@@ -125,6 +87,60 @@ void ArticleContainer::SetupSizer()
 	m_pTopSizer->Add(m_pArticleList, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
 }
 
+// ArticleNotebook
+
+ArticleNotebook::ArticleNotebook(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+	: wxNotebook(parent, id, pos, size, style)
+{
+	this->Init();
+}
+
+ArticleNotebook::~ArticleNotebook()
+{
+	this->DestroyChildren();
+}
+
+void ArticleNotebook::Init()
+{
+	this->SetupControls();
+	this->SetupPanels();
+	this->SetupSizer();
+	this->SetupNotebook();
+}
+
+void ArticleNotebook::SetupControls()
+{
+	m_pFitnessContainer = new ArticleContainer(_T("Health && Fitness"), this, wxID_ANY);
+	m_pFocusContainer = new ArticleContainer(_T("Focus && Productivity"), this, wxID_ANY);
+	m_pNutritionContainer = new ArticleContainer(_T("Food && Nutrition"), this, wxID_ANY);
+	
+}
+
+void ArticleNotebook::SetupPanels()
+{
+}
+
+void ArticleNotebook::SetupSizer()
+{
+}
+
+void ArticleNotebook::SetupNotebook()
+{
+	// Set up the image list
+	m_pImageList = new wxImageList(30, 30);
+
+	m_fitnessBmp = wxBitmap(path_data::dataDir + _T("\\Images\\Articles\\run.png"), wxBITMAP_TYPE_PNG);
+	m_focusBmp = wxBitmap(path_data::dataDir + _T("\\Images\\Articles\\growth.png"), wxBITMAP_TYPE_PNG);
+
+	m_pImageList->Add(m_fitnessBmp);
+	m_pImageList->Add(m_focusBmp);
+	this->AssignImageList(m_pImageList);
+
+	// Add pages
+	this->AddPage(m_pFitnessContainer, _T("Health && Fitness"), true, 0);
+	this->AddPage(m_pFocusContainer, _T("Focus && Productivity"), false, 1);
+}
+
 // ArticlePanel
 
 ArticlePanel::ArticlePanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
@@ -140,6 +156,7 @@ ArticlePanel::~ArticlePanel()
 
 void ArticlePanel::SetupArticlePanel()
 {
+	m_pArticleNotebook = new ArticleNotebook(this, wxID_ANY);
 	this->SetupSizers();
 }
 
@@ -149,5 +166,7 @@ void ArticlePanel::SetupSizers()
 	// Initialize the top sizer
 	m_pTopSizer = new wxBoxSizer(wxVERTICAL);
 	this->SetSizerAndFit(m_pTopSizer);
+
+	m_pTopSizer->Add(m_pArticleNotebook, wxSizerFlags().Proportion(1).Expand().Border(wxALL, 5));
 }
 
